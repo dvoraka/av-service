0dd550d8744e0fa098f7b79164dcda18719a761c
dvoraka <alen.dvorak@gmail.com>
2016-02-06 00:11:22 +0100

    Add first test

---
+++ src/test/groovy/dvoraka/avservice/AVServiceSpec.groovy
@@ -0,0 +1,14 @@
+package dvoraka.avservice
+
+import spock.lang.Specification
+
+/**
+ * Specification class for AVService.
+ */
+class AVServiceSpec extends Specification {
+
+    def "default test"() {
+        expect:
+        true
+    }
+}
