10c107c7f9367712e861b78650415903466891ed
dvoraka <alen.dvorak@gmail.com>
2016-02-07 01:27:12 +0100

    Improve testing

--- src/test/groovy/dvoraka/avservice/AVServiceSpec.groovy
+++ src/test/groovy/dvoraka/avservice/AVServiceSpec.groovy
@@ -1,14 +1,25 @@
 package dvoraka.avservice
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 /**
  * Specification class for AVService.
  */
+@ContextConfiguration(classes = [AppConfig])
 class AVServiceSpec extends Specification {
+
+    @Autowired
+    AVService avService;
 
     def "default test"() {
         expect:
         true
+    }
+
+    def "AV service loading"() {
+        expect:
+        avService != null
     }
 }
