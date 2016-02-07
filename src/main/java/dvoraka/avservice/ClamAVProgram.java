9cb6307d71d22a3b12dbbd92ee546a6f4fad3065
dvoraka <alen.dvorak@gmail.com>
2016-02-08 00:40:40 +0100

    Prepare anti-virus implementation

---
+++ src/main/java/dvoraka/avservice/ClamAVProgram.java
@@ -0,0 +1,48 @@
+package dvoraka.avservice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * ClamAV wrapper.
+ */
+public class ClamAVProgram implements AVProgram {
+
+    private String socketHost = "localhost";
+    private int socketPort = 3310;
+
+
+    public static void main(String[] args) throws IOException {
+
+        ClamAVProgram prog = new ClamAVProgram();
+        System.out.println("Clamav ping test");
+        System.out.println("Result: " + prog.ping());
+    }
+
+    @Override
+    public boolean scanStream(byte[] bytes) {
+        return false;
+    }
+
+    public boolean ping() {
+
+        String response = null;
+        try (
+                Socket socket = new Socket(socketHost, socketPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ) {
+            out.println("nPING");
+            out.flush();
+            response = in.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ("PONG".equals(response));
+    }
+}
