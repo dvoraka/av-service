b5a294670f80375a83e1f4b63f7cca03c5a866db
dvoraka <alen.dvorak@gmail.com>
2016-02-08 02:18:52 +0100

    Improve ClamAV wrapper

--- src/main/java/dvoraka/avservice/ClamAVProgram.java
+++ src/main/java/dvoraka/avservice/ClamAVProgram.java
@@ -3,8 +3,11 @@ package dvoraka.avservice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 /**
  * ClamAV wrapper.
@@ -23,6 +26,10 @@ public class ClamAVProgram implements AVProgram {
         ClamAVProgram prog = new ClamAVProgram();
         System.out.println("Clamav ping test");
         System.out.println("Result: " + prog.ping());
+        System.out.println("Clamav version");
+        System.out.println("Result: " + prog.version());
+        System.out.println("Clamav checking data");
+        System.out.println("Result: " + prog.scanStream("aaa".getBytes()));
     }
 
     public ClamAVProgram() {
@@ -36,6 +43,30 @@ public class ClamAVProgram implements AVProgram {
 
     @Override
     public boolean scanStream(byte[] bytes) {
+
+        String response = null;
+        try (
+                Socket socket = new Socket(socketHost, socketPort);
+                OutputStream outStream = socket.getOutputStream();
+                InputStreamReader inReader = new InputStreamReader(socket.getInputStream());
+                BufferedReader in = new BufferedReader(inReader)
+        ) {
+            byte[] lenghtBytes = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(bytes.length).array();
+            outStream.write("nINSTREAM\n".getBytes("UTF-8"));
+            outStream.write(lenghtBytes);
+            outStream.write(bytes);
+
+            byte[] zeroLengthBytes = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(0).array();
+            outStream.write(zeroLengthBytes);
+            outStream.flush();
+
+            response = in.readLine();
+            System.out.println(response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
@@ -57,5 +88,25 @@ public class ClamAVProgram implements AVProgram {
         }
 
         return "PONG".equals(response);
+    }
+
+    public String version() {
+
+        String version = null;
+        try (
+                Socket socket = new Socket(socketHost, socketPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                InputStreamReader inReader = new InputStreamReader(socket.getInputStream());
+                BufferedReader in = new BufferedReader(inReader)
+        ) {
+            out.println("nVERSION");
+            out.flush();
+            version = in.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return version;
     }
 }
