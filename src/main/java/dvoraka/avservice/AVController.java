2732823ee787ff2a5237a8299f145dd243319ae8
Alen Dvorak <alen.dvorak@gmail.com>
2016-03-11 01:56:54 +0100

    Add some testing functionality for REST

--- src/main/java/dvoraka/avservice/AVController.java
+++ src/main/java/dvoraka/avservice/AVController.java
@@ -1,18 +1,35 @@
 package dvoraka.avservice;
 
+import dvoraka.avservice.data.AVMessage;
+import dvoraka.avservice.data.AVMessageType;
+import dvoraka.avservice.data.DefaultAVMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Spring REST prototype
+ * Spring REST prototype.
  */
 @RestController
 public class AVController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/")
     public String info(@RequestParam(value = "info", defaultValue = "AV service") String info) {
 
         return info;
+    }
+
+    /**
+     * Generates testing AV message.
+     *
+     * @return the generated message
+     */
+    @RequestMapping(value = "/genmsg")
+    public AVMessage generateMessage() {
+
+        return new DefaultAVMessage.Builder(null)
+                .serviceId("test-service")
+                .type(AVMessageType.RESPONSE)
+                .build();
     }
 }
