dd735d91e9c2a21424db90fadce1565d4af794f4
Alen Dvorak <alen.dvorak@gmail.com>
2016-03-13 21:42:07 +0100

    Improve REST

--- src/main/java/dvoraka/avservice/rest/AVController.java
+++ src/main/java/dvoraka/avservice/rest/AVController.java
@@ -6,14 +6,17 @@ import dvoraka.avservice.data.AVMessageType;
 import dvoraka.avservice.data.DefaultAVMessage;
 import dvoraka.avservice.service.RestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Spring REST prototype.
+ * AV REST controller.
  */
 @RestController
 public class AVController {
@@ -21,9 +24,14 @@ public class AVController {
     @Autowired
     private RestService restService;
 
+    /**
+     * Testing method.
+     *
+     * @param info the info String
+     * @return the info
+     */
     @RequestMapping(value = "/")
     public String info(@RequestParam(value = "info", defaultValue = "AV service") String info) {
-
         return info;
     }
 
@@ -38,22 +46,42 @@ public class AVController {
         return restService.messageStatus(id);
     }
 
+    /**
+     * Returns the message status.
+     *
+     * @param id        the message ID
+     * @param serviceId the service ID
+     * @return the status
+     */
     @RequestMapping(value = "/msg-status/{id}/{serviceId}", method = RequestMethod.GET)
     public MessageStatus messageStatus(@PathVariable String id, @PathVariable String serviceId) {
         return restService.messageStatus(id);
     }
 
+    /**
+     * Returns the service ID for the message ID.
+     *
+     * @param id the message ID
+     * @return the service ID
+     */
     @RequestMapping(value = "/msg-service-id/{id}", method = RequestMethod.GET)
     public String messageServiceId(@PathVariable String id) {
         return restService.messageServiceId(id);
     }
 
+    @RequestMapping(value = "/msg-check", method = RequestMethod.POST)
+    public ResponseEntity<String> messageCheck(@RequestBody DefaultAVMessage message) {
+
+        System.out.println(message);
+        return new ResponseEntity<String>(HttpStatus.ACCEPTED);
+    }
+
     /**
-     * Generates testing AV message.
+     * Generates a testing AV message.
      *
      * @return the generated message
      */
-    @RequestMapping(value = "/genmsg")
+    @RequestMapping(value = "/gen-msg")
     public AVMessage generateMessage() {
 
         return new DefaultAVMessage.Builder(null)
--- src/main/java/dvoraka/avservice/rest/RestClient.java
+++ src/main/java/dvoraka/avservice/rest/RestClient.java
@@ -12,7 +12,14 @@ public class RestClient {
 
         String url = "http://localhost:8080/av-service";
         RestTemplate restTemplate = new RestTemplate();
-        DefaultAVMessage message = restTemplate.getForObject(url + "/genmsg.json", DefaultAVMessage.class);
+        DefaultAVMessage message = restTemplate.getForObject(url + "/gen-msg.json", DefaultAVMessage.class);
         System.out.println(message);
+
+        DefaultAVMessage msgToUpload = new DefaultAVMessage.Builder("TESTING-REQUEST")
+                .data(new byte[10])
+                .serviceId("SERVICE1")
+                .build();
+        System.out.println("Sent msg: " + msgToUpload);
+        restTemplate.postForObject(url + "/msg-check", msgToUpload, DefaultAVMessage.class);
     }
 }
