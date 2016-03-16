b8a216d6068cf2264bacd3191a7833a8955e64e9
dvoraka <alen.dvorak@gmail.com>
2016-03-16 02:15:52 +0100

    Improve CI configuration

--- travis-config-deps.sh
+++ travis-config-deps.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 # set TCP socket for ClamAV daemon
+sed -i 's/^LocalSocket.*$/#/' /etc/clamav/clamd.conf
 echo "TCPSocket 3310" >> /etc/clamav/clamd.conf
 echo "TCPAddr 127.0.0.1" >> /etc/clamav/clamd.conf
 
