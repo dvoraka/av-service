b5ba8bf59e7ace740bb2a9d0a72844501434fbfe
Alen Dvorak <alen.dvorak@gmail.com>
2016-03-16 15:16:29 +0100

    Implement scanFile method

--- src/main/java/dvoraka/avservice/service/DefaultAVService.java
+++ src/main/java/dvoraka/avservice/service/DefaultAVService.java
@@ -1,26 +1,48 @@
 package dvoraka.avservice.service;
 
 import dvoraka.avservice.AVProgram;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Default AV service implementation.
  */
 public class DefaultAVService implements AVService {
 
+    private static final Logger log = LogManager.getLogger(DefaultAVService.class.getName());
+
     @Autowired
     private AVProgram avProgram;
+
 
     @Override
     public boolean scanStream(byte[] bytes) {
         return avProgram.scanStream(bytes);
     }
 
-    // TODO: implement
     @Override
     public boolean scanFile(File file) {
-        return true;
+        // TODO: check file size
+        byte[] bytes = null;
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            // TODO: throw exception
+            log.warn("File error!", e);
+        }
+
+        boolean result = false;
+        if (bytes != null) {
+            result = scanStream(bytes);
+        } else {
+            // TODO: throw exception
+        }
+
+        return result;
     }
 }
