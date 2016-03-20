3221d1a3098cfea5efce92c18986d8409499e3b6
Alen Dvorak <alen.dvorak@gmail.com>
2016-03-20 01:11:01 +0100

    Prepare message cache for REST

--- src/main/java/dvoraka/avservice/rest/DirectRestStrategy.java
+++ src/main/java/dvoraka/avservice/rest/DirectRestStrategy.java
@@ -3,7 +3,15 @@ package dvoraka.avservice.rest;
 import dvoraka.avservice.MessageProcessor;
 import dvoraka.avservice.data.AVMessage;
 import dvoraka.avservice.data.MessageStatus;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.expiry.Duration;
+import org.ehcache.expiry.Expirations;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Implementation for a local calling.
@@ -13,6 +21,26 @@ public class DirectRestStrategy implements RestStrategy {
     @Autowired
     private MessageProcessor messageProcessor;
 
+    CacheManager cacheManager;
+    Cache<String, AVMessage> messageCache;
+
+
+    public DirectRestStrategy() {
+
+        initializeCache();
+    }
+    private void initializeCache() {
+        cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("restCache",
+                        CacheConfigurationBuilder
+                                .newCacheConfigurationBuilder(String.class, AVMessage.class)
+                                .withExpiry(Expirations.timeToLiveExpiration(
+                                        new Duration(1000, TimeUnit.MILLISECONDS)))
+                                .build())
+                .build(true);
+
+        messageCache = cacheManager.getCache("restCache", String.class, AVMessage.class);
+    }
 
     @Override
     public MessageStatus messageStatus(String id) {
@@ -47,5 +75,7 @@ public class DirectRestStrategy implements RestStrategy {
     @Override
     public void stop() {
         messageProcessor.stop();
+
+        cacheManager.close();
     }
 }
