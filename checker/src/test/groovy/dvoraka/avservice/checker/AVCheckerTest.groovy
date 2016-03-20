55169b4393f35d0c75002ca50cc8306c419dea3d
Alen Dvorak <alen.dvorak@gmail.com>
2016-03-20 18:45:30 +0100

    Improve build dependencies

--- checker/src/test/groovy/dvoraka/avservice/checker/AVCheckerTest.groovy
+++ checker/src/test/groovy/dvoraka/avservice/checker/AVCheckerTest.groovy
@@ -2,7 +2,7 @@ package dvoraka.avservice.checker
 
 import dvoraka.avservice.checker.receiver.Receiver
 import dvoraka.avservice.checker.sender.Sender
-import spock.lang.*;
+import spock.lang.Specification;
 
 
 class AVCheckerTest extends Specification {
@@ -10,15 +10,15 @@ class AVCheckerTest extends Specification {
     def "test check calls"() {
 
         setup:
-            Sender sender = Mock()
-            Receiver receiver = Mock()
-            AVChecker checker = new AVChecker(sender, receiver)
+        Sender sender = Mock()
+        Receiver receiver = Mock()
+        AVChecker checker = new AVChecker(sender, receiver)
 
         when:
-            checker.check()
+        checker.check()
 
         then:
-            1 * sender.sendFile(_, _)
-            1 * receiver.receive(_)
+        1 * sender.sendFile(_, _)
+        1 * receiver.receive(_)
     }
 }
