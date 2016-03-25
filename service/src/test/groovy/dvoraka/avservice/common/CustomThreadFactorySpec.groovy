469ce64ed073fdce4a21ee258b011c5ec66b2dd8
Alen Dvorak <alen.dvorak@gmail.com>
2016-03-25 17:25:08 +0100

    Add thread factory test

---
+++ service/src/test/groovy/dvoraka/avservice/common/CustomThreadFactorySpec.groovy
@@ -0,0 +1,33 @@
+package dvoraka.avservice.common
+
+import spock.lang.Specification
+
+/**
+ * Custom thread factory test.
+ */
+class CustomThreadFactorySpec extends Specification {
+
+    def "test factory"() {
+        setup:
+        String name = 'TEST-POOl-'
+        CustomThreadFactory factory = new CustomThreadFactory(name)
+
+        Thread thread1 = factory.newThread(new Runnable() {
+            @Override
+            void run() {
+            }
+        })
+
+        Thread thread2 = factory.newThread(new Runnable() {
+            @Override
+            void run() {
+            }
+        })
+
+        expect:
+        thread1.getName().startsWith(name)
+        thread2.getName().startsWith(name)
+
+        !thread1.getName().equals(thread2.getName())
+    }
+}
