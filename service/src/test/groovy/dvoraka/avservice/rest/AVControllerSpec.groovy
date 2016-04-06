dadcb1c9cefeda764cb98260b33b7bc78e58d876
Alen Dvorak <alen.dvorak@gmail.com>
2016-04-06 20:55:13 +0200

    Prepare unit test for the REST controller

---
+++ service/src/test/groovy/dvoraka/avservice/rest/AVControllerSpec.groovy
@@ -0,0 +1,56 @@
+package dvoraka.avservice.rest
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import dvoraka.avservice.common.data.MessageStatus
+import dvoraka.avservice.service.RestService
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+/**
+ * AV controller test.
+ */
+class AVControllerSpec extends Specification {
+
+    MockMvc mockMvc;
+
+    def setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new AVController()).build()
+    }
+
+    def "test info"() {
+        setup:
+        ResultActions response = mockMvc.perform(get("/"))
+
+        expect:
+        response
+                .andExpect(status().isOk())
+                .andExpect(content().string("AV service"))
+    }
+
+    def "test mesageStatus(String)"() {
+        setup:
+        MessageStatus messageStatus = MessageStatus.WAITING
+        RestService service = Stub()
+        service.messageStatus(_) >> messageStatus
+
+        mockMvc = MockMvcBuilders.standaloneSetup(
+                new AVController(restService: service)).build()
+
+        String messageId = 'TID'
+        ResultActions response = mockMvc.perform(get("/msg-status/${messageId}"))
+
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedContent = mapper.writeValueAsString(messageStatus)
+
+        expect:
+        response
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedContent))
+    }
+}
