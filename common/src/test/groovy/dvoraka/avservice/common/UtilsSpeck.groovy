ffb835717ff9521f2f41bb087127a6a3ff734000
Alen Dvorak <alen.dvorak@gmail.com>
2016-04-16 13:03:26 +0200

    Add Utils test (see #23)

---
+++ common/src/test/groovy/dvoraka/avservice/common/UtilsSpeck.groovy
@@ -0,0 +1,40 @@
+package dvoraka.avservice.common
+
+import dvoraka.avservice.common.data.AVMessage
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+
+/**
+ * Utils test.
+ */
+class UtilsSpeck extends Specification {
+
+
+    def "generate normal message"() {
+        setup:
+        AVMessage message = Utils.genNormalMessage()
+
+        expect:
+        checkMessageFields(message)
+        message.getVirusInfo()?.equals('UNKNOWN')
+    }
+
+    def "generate infected message"() {
+        setup:
+        AVMessage message = Utils.genInfectedMessage()
+        byte[] expectedData = message.getData()
+
+        expect:
+        checkMessageFields(message)
+        Arrays.equals(expectedData, Utils.EICAR.getBytes(StandardCharsets.UTF_8))
+    }
+
+    void checkMessageFields(AVMessage message) {
+        assert message.getId()
+        assert message.getCorrelationId()
+        assert message.getData()
+        assert message.getType()
+        assert message.getServiceId()
+    }
+}
