baa78585c8f1c776fc5eb2c131d1f32c747b31e2
Alen Dvorak <alen.dvorak@gmail.com>
2016-04-19 21:29:28 +0200

    Prepare distribution

--- checker/bin/check.sh
+++ checker/bin/check.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
-echo "Checker"
+SCRIPT_DIR=$(readlink -e "${BASH_SOURCE}")
+echo $SCRIPT_DIR
+BASE_DIR=$(dirname -- "${SCRIPT_DIR}")
+echo $BASE_DIR
+
+JAVA_OPTS=""
+LOGGING_OPTS=""
+
+LIB_PATH="${BASE_DIR}"/lib
+echo $LIB_PATH
+
 
 exit 0
