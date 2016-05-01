961c614779a55dcf73e7995fb6add8b8d64556e1
Alen Dvorak <alen.dvorak@gmail.com>
2016-05-01 18:18:23 +0200

    Create a test for RabbitMQ configuration

---
+++ service/src/integration-test/groovy/dvoraka/avservice/RabbitMQConfiguration.groovy
@@ -0,0 +1,38 @@
+package dvoraka.avservice
+
+import dvoraka.avservice.common.Utils
+import dvoraka.avservice.common.data.AVMessage
+import dvoraka.avservice.common.data.AVMessageMapper
+import dvoraka.avservice.configuration.AppConfig
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Ignore
+import spock.lang.Specification
+
+/**
+ * Configure RabbitMQ. Creates necessary queues and bindings.
+ */
+@Ignore
+@ContextConfiguration(classes = [AppConfig])
+@ActiveProfiles("amqp")
+class RabbitMQConfiguration extends Specification {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate
+
+
+    def "send message"() {
+        setup:
+        AVMessage avMessage = Utils.genNormalMessage()
+        Message message = AVMessageMapper.transform(avMessage)
+
+        when:
+        rabbitTemplate.sendAndReceive(message)
+
+        then:
+        notThrown(Exception)
+    }
+}
