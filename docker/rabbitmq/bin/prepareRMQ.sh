e26c21717fdea3e0abb5f5c17d16d46549e56928
Alen Dvorak <alen.dvorak@gmail.com>
2016-05-01 16:54:42 +0200

    Update Docker configuration

---
+++ docker/rabbitmq/bin/prepareRMQ.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Prepares basic RabbitMQ structure
+#
+
+# add user guest with password guest
+rabbitmqctl add_user guest guest
+
+# create virtual host antivirus
+rabbitmqctl add_vhost antivirus
+
+# set all permissions on antivirus for user guest
+rabbitmqctl set_permissions -p antivirus guest ".*" ".*" ".*"
+
+
+exit 0
