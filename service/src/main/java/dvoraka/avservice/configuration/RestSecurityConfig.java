fadca8f0b750434c713579e542037dd1ad8d7eae
Alen Dvorak <alen.dvorak@gmail.com>
2016-05-14 16:40:13 +0200

    Prepare REST security (see #31)

--- service/src/main/java/dvoraka/avservice/configuration/RestSecurityConfig.java
+++ service/src/main/java/dvoraka/avservice/configuration/RestSecurityConfig.java
@@ -1,10 +1,13 @@
 package dvoraka.avservice.configuration;
 
+import dvoraka.avservice.service.BasicUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Rest security configuration.
@@ -13,6 +16,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new BasicUserDetailsService();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,5 +35,7 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("guest").password("guest").roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER", "ADMIN");
+
+        auth.userDetailsService(userDetailsService());
     }
 }
---
+++ service/src/main/java/dvoraka/avservice/service/BasicUserDetailsService.java
@@ -0,0 +1,28 @@
+package dvoraka.avservice.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User details service implementation.
+ */
+public class BasicUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username.equals("JOHN")) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+            return new User("JOHN", "PASS", true, true, true, true, authorities);
+        } else {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+    }
+}
