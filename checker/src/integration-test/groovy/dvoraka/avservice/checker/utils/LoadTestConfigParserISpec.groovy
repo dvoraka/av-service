95fbfa4e0055afc38aeaf63209ccee11c716b931
Alen Dvorak <alen.dvorak@gmail.com>
2016-06-26 17:46:04 +0200

    Improve the parser test (see #33)

--- checker/src/integration-test/groovy/dvoraka/avservice/checker/utils/LoadTestConfigParserISpec.groovy
+++ checker/src/integration-test/groovy/dvoraka/avservice/checker/utils/LoadTestConfigParserISpec.groovy
@@ -32,4 +32,34 @@ class LoadTestConfigParserISpec extends Specification {
         props.get("synchronous").equals("true")
         props.get("virtualHost").equals("AntivirusTest")
     }
+
+    def "parsing test with a parse error"() {
+        given:
+        String filename = getClass().getResource("/loadTestTestingFileWithError.xml").toString()
+        Map<String, String> props
+
+        when:
+        configParser.parseFileSax(filename)
+        props = configParser.getProperties()
+
+        then:
+        notThrown(Exception)
+        props.size() == 2
+        props.get("host").equals("TestHost")
+        props.get("virtualHost").equals("AntivirusTest")
+    }
+
+    def "parsing test without a file"() {
+        given:
+        String filename = getClass().getResource("/someRandomFilename1112.xml").toString()
+        Map<String, String> props
+
+        when:
+        configParser.parseFileSax(filename)
+        props = configParser.getProperties()
+
+        then:
+        notThrown(Exception)
+        props.size() == 0
+    }
 }
