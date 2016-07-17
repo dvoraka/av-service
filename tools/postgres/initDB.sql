159c8c31351340066831eb0ae8bf9c7ddf221ff3
Alen Dvorak <alen.dvorak@gmail.com>
2016-07-17 18:42:31 +0200

    Tune initDB script

--- tools/postgres/initDB.sql
+++ tools/postgres/initDB.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS public.car_info;
 
 CREATE TABLE public.car (
     id INTEGER PRIMARY KEY,
-    name varchar (
-        50 ) );
+    name VARCHAR(50)
+);
 
 CREATE TABLE public.car_info (
     id INTEGER PRIMARY KEY,
-    info varchar (
-        500 ),
-    fk_car_info_car INT NOT NULL REFERENCES public.car );
+    info VARCHAR(500),
+    car_id INT NOT NULL
+);
+ALTER TABLE public.car_info ADD CONSTRAINT fk_car_info_car FOREIGN KEY (car_id) REFERENCES public.car(id);
