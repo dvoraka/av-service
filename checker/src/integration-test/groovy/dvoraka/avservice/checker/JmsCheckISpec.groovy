93f3e1942aafa70790cd667b1c33475d01ab5697
Alen Dvorak <alen.dvorak@gmail.com>
2016-07-22 17:15:57 +0200

    Add JMS infrastructure test

---
+++ checker/src/integration-test/groovy/dvoraka/avservice/checker/JmsCheckISpec.groovy
@@ -0,0 +1,47 @@
+package dvoraka.avservice.checker
+
+import dvoraka.avservice.common.Utils
+import dvoraka.avservice.common.data.AvMessage
+import dvoraka.avservice.server.AvServer
+import dvoraka.avservice.server.configuration.JmsConfig
+import dvoraka.avservice.server.jms.JmsClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * Temporary JMS infrastructure test.
+ */
+@ContextConfiguration(classes = [JmsConfig.class])
+@ActiveProfiles(["jms", "jms-async"])
+class JmsCheckISpec extends Specification {
+
+    @Autowired
+    AvServer basicAvServer
+    @Autowired
+    JmsClient jmsClient
+
+
+    def setup() {
+        if (!basicAvServer.isRunning()) {
+            basicAvServer.start()
+        }
+    }
+
+    def cleanup() {
+    }
+
+    def "send and receive infected AvMessage"() {
+        given:
+            AvMessage infectedMsg = Utils.genInfectedMessage()
+
+        when:
+            jmsClient.sendMessage(infectedMsg, "check")
+            AvMessage receivedMsg = jmsClient.receiveMessage("result")
+
+        then:
+            receivedMsg
+            receivedMsg.getCorrelationId() == infectedMsg.getId()
+    }
+}
