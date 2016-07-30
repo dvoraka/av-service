ee0e9eef6e62b5a22ebe9e7615cea5375176716e
Alen Dvorak <alen.dvorak@gmail.com>
2016-07-30 19:47:19 +0200

    Improve AvProgram docstrings

--- service/src/main/java/dvoraka/avservice/avprogram/AvProgram.java
+++ service/src/main/java/dvoraka/avservice/avprogram/AvProgram.java
@@ -7,9 +7,42 @@ import dvoraka.avservice.common.exception.ScanErrorException;
  */
 public interface AvProgram {
 
+    /**
+     * Scans the byte stream and returns the infected flag.
+     *
+     * @param bytes byte stream for scanning
+     * @return the infected flag
+     * @throws ScanErrorException when scanning failed
+     */
     boolean scanStream(byte[] bytes) throws ScanErrorException;
 
+    /**
+     * Scans the byte stream and returns an info about found viruses.
+     *
+     * @param bytes byte stream for scanning
+     * @return the virus description
+     * @throws ScanErrorException when scanning failed
+     */
     String scanStreamWithInfo(byte[] bytes) throws ScanErrorException;
 
+    /**
+     * Returns status of the program.
+     *
+     * @return the running status
+     */
     boolean isRunning();
+
+    /**
+     * Sets the caching for the program.
+     *
+     * @param caching enable/disable caching
+     */
+    void setCaching(boolean caching);
+
+    /**
+     * Returns maximum stream size.
+     *
+     * @return the maximum stream size
+     */
+    long getMaxSize();
 }
