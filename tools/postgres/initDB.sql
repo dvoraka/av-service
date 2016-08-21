bf6c9ad7608111f9923d1d0ab3599ec1e71fc517
Alen Dvorak <alen.dvorak@gmail.com>
2016-08-21 15:07:28 +0200

    Improve SQL init script

--- tools/postgres/initDB.sql
+++ tools/postgres/initDB.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS public.car CASCADE;
 DROP TABLE IF EXISTS public.car_info;
 
+DROP TABLE IF EXISTS public.user CASCADE;
+DROP TABLE IF EXISTS public.authority;
+
 CREATE TABLE public.car (
   id   INTEGER PRIMARY KEY,
   name VARCHAR(50)
@@ -13,3 +16,16 @@ CREATE TABLE public.car_info (
 );
 ALTER TABLE public.car_info
   ADD CONSTRAINT fk_car_info_car FOREIGN KEY (car_id) REFERENCES public.car (id);
+
+CREATE TABLE public.user (
+  username VARCHAR(50) NOT NULL PRIMARY KEY,
+  password VARCHAR(50) NOT NULL,
+  enabled  BOOLEAN     NOT NULL
+);
+
+CREATE TABLE public.authority (
+  username  VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL
+);
+ALTER TABLE public.authority
+  ADD CONSTRAINT fk_authority_user FOREIGN KEY (username) REFERENCES public.user (username);
