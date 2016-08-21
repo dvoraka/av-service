a1dd05e85cc400f4ace5f3ff58c8ff594e40d8b6
Alen Dvorak <alen.dvorak@gmail.com>
2016-08-21 14:41:31 +0200

    Reformat SQL init script

--- tools/postgres/initDB.sql
+++ tools/postgres/initDB.sql
@@ -2,13 +2,14 @@ DROP TABLE IF EXISTS public.car CASCADE;
 DROP TABLE IF EXISTS public.car_info;
 
 CREATE TABLE public.car (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(50)
+  id   INTEGER PRIMARY KEY,
+  name VARCHAR(50)
 );
 
 CREATE TABLE public.car_info (
-    id INTEGER PRIMARY KEY,
-    info VARCHAR(500),
-    car_id INT NOT NULL
+  id     INTEGER PRIMARY KEY,
+  info   VARCHAR(500),
+  car_id INT NOT NULL
 );
-ALTER TABLE public.car_info ADD CONSTRAINT fk_car_info_car FOREIGN KEY (car_id) REFERENCES public.car(id);
+ALTER TABLE public.car_info
+  ADD CONSTRAINT fk_car_info_car FOREIGN KEY (car_id) REFERENCES public.car (id);
