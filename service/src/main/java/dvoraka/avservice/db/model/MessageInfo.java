f1cc73dea2baa53490ad674a5bb25edc8a4146ee
Alen Dvorak <alen.dvorak@gmail.com>
2016-09-02 16:07:00 +0200

    Prepare message logging (see #10)

---
+++ service/src/main/java/dvoraka/avservice/db/model/MessageInfo.java
@@ -0,0 +1,58 @@
+package dvoraka.avservice.db.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+/**
+ * Message info entity.
+ */
+@Entity
+public class MessageInfo {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String uuid;
+    private String source;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Date getCreated() {
+        return new Date(created.getTime());
+    }
+
+    public void setCreated(Date created) {
+        this.created = new Date(created.getTime());
+    }
+}
