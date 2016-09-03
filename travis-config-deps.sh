99634a0789011570b616a7c4b528b5d3e6f0ff11
Alen Dvorak <alen.dvorak@gmail.com>
2016-09-03 16:20:32 +0200

    Update Travis config and remove unnecessary config files

--- travis-config-deps.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# enable TCP socket for ClamAV daemon
-echo "TCPSocket 3310" >> /etc/clamav/clamd.conf
-echo "TCPAddr 127.0.0.1" >> /etc/clamav/clamd.conf
-
-# enable main instance for ActiveMQ
-ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/main
