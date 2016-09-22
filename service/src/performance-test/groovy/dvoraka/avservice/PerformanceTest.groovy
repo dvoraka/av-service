7cb73a8d293eac10dc908d63a04329c9376140b1
Alen Dvorak <alen.dvorak@gmail.com>
2016-09-22 20:29:05 +0200

    #12 Add configuration and template performance test

---
+++ service/src/performance-test/groovy/dvoraka/avservice/PerformanceTest.groovy
@@ -0,0 +1,11 @@
+package dvoraka.avservice
+
+import spock.lang.Specification
+
+class PerformanceTest extends Specification {
+
+    def "template"() {
+        expect:
+            println("Performance test!")
+    }
+}
