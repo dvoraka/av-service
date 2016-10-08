ec5d002c4ad8bf480a995b8979317cd834fb3088
Alen Dvorak <alen.dvorak@gmail.com>
2016-10-09 01:51:40 +0200

    #85 Implement saveSoft method

--- database/src/main/java/dvoraka/avservice/db/repository/SolrMessageInfoRepositoryImpl.java
+++ database/src/main/java/dvoraka/avservice/db/repository/SolrMessageInfoRepositoryImpl.java
@@ -1,6 +1,8 @@
 package dvoraka.avservice.db.repository;
 
 import dvoraka.avservice.db.model.MessageInfoDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.core.SolrOperations;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,10 +11,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class SolrMessageInfoRepositoryImpl implements SolrMessageInfoRepositoryCustom {
 
+    @Autowired
+    private SolrOperations operations;
+
+
     @Override
     public MessageInfoDocument saveSoft(MessageInfoDocument document) {
-        System.out.println("Save soft");
+        operations.saveBean(document);
+        operations.softCommit();
 
-        return null;
+        return document;
     }
 }
