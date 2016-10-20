484920af7aedd6c93c901fce8b8dfb2b13a4aac8
Alen Dvorak <alen.dvorak@gmail.com>
2016-10-20 20:50:44 +0200

    #68 Improve configuration test

--- server/src/integration-test/groovy/dvoraka/avservice/server/configuration/AmqpConfigISpec.groovy
+++ server/src/integration-test/groovy/dvoraka/avservice/server/configuration/AmqpConfigISpec.groovy
@@ -1,138 +1,24 @@
 package dvoraka.avservice.server.configuration
 
-import dvoraka.avservice.MessageProcessor
 import dvoraka.avservice.server.AvServer
-import dvoraka.avservice.server.ServerComponent
-import org.springframework.amqp.core.AmqpAdmin
-import org.springframework.amqp.core.Binding
-import org.springframework.amqp.core.FanoutExchange
-import org.springframework.amqp.core.MessageListener
-import org.springframework.amqp.core.Queue
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 /**
- * AMQP Spring configuration test.
+ * Configuration test.
  */
 @ContextConfiguration(classes = [AmqpConfig.class])
-@ActiveProfiles(["amqp", "database"])
+@ActiveProfiles(["amqp", "no-db"])
 class AmqpConfigISpec extends Specification {
 
     @Autowired
     AvServer avServer
 
-    @Autowired
-    ServerComponent serverComponent
 
-    @Autowired
-    MessageProcessor messageProcessor
-
-    @Autowired
-    SimpleMessageListenerContainer messageListenerContainer
-
-    @Autowired
-    ConnectionFactory connectionFactory
-
-    @Autowired
-    AmqpAdmin amqpAdmin
-
-    @Autowired
-    RabbitTemplate rabbitTemplate
-
-    @Autowired
-    MessageListener messageListener
-
-    @Autowired
-    Queue checkQueue
-
-    @Autowired
-    Queue resultQueue
-
-    @Autowired
-    FanoutExchange checkExchange
-
-    @Autowired
-    FanoutExchange resultExchange
-
-    @Autowired
-    Binding bindingCheck
-
-    @Autowired
-    Binding bindingResult
-
-
-    def "AvServer loading"() {
+    def "test"() {
         expect:
-            avServer
-    }
-
-    def "ServerComponent loading"() {
-        expect:
-            serverComponent
-    }
-
-    def "MessageProcessor loading"() {
-        expect:
-            messageProcessor
-    }
-
-    def "SimpleMessageListenerContainer loading"() {
-        expect:
-            messageListenerContainer
-    }
-
-    def "ConnectionFactory loading"() {
-        expect:
-            connectionFactory
-    }
-
-    def "AmqpAdmin loading"() {
-        expect:
-            amqpAdmin
-    }
-
-    def "RabbitTemplate loading"() {
-        expect:
-            rabbitTemplate
-    }
-
-    def "MessageListener loading"() {
-        expect:
-            messageListener
-    }
-
-    def "Queue check loading"() {
-        expect:
-            checkQueue
-    }
-
-    def "Queue result loading"() {
-        expect:
-            resultQueue
-    }
-
-    def "FanoutExchange check loading"() {
-        expect:
-            checkExchange
-    }
-
-    def "FanoutExchange result loading"() {
-        expect:
-            resultExchange
-    }
-
-    def "Binding check loading"() {
-        expect:
-            bindingCheck
-    }
-
-    def "Binding result loading"() {
-        expect:
-            bindingResult
+            true
     }
 }
