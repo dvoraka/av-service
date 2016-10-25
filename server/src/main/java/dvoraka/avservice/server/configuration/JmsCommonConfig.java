9b9140d94afe0eb383dadfca8a0cd10b626f38dd
Alen Dvorak <alen.dvorak@gmail.com>
2016-10-25 22:31:35 +0200

    #102 Configuration redesign

--- server/src/main/java/dvoraka/avservice/server/configuration/JmsCommonConfig.java
+++ server/src/main/java/dvoraka/avservice/server/configuration/JmsCommonConfig.java
@@ -1,8 +1,21 @@
 package dvoraka.avservice.server.configuration;
 
+import dvoraka.avservice.server.ServerComponent;
+import dvoraka.avservice.server.jms.JmsComponent;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.listener.SimpleMessageListenerContainer;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageListener;
 
 /**
  * JMS common configuration.
@@ -11,4 +24,77 @@ import org.springframework.context.annotation.Profile;
 @Import({ServerCommonConfig.class})
 @Profile("jms")
 public class JmsCommonConfig {
+
+    @Value("${avservice.jms.brokerUrl}")
+    private String brokerUrl;
+
+    @Value("${avservice.jms.checkDestination:check}")
+    private String checkDestination;
+
+    @Value("${avservice.jms.resultDestination:result}")
+    private String resultDestination;
+
+    @Value("${avservice.serviceId:default1")
+    private String serviceId;
+
+    @Value("${avservice.jms.receiveTimeout:2000}")
+    private long receiveTimeout;
+
+
+    @Bean
+    public ServerComponent serverComponent() {
+        return new JmsComponent(resultDestination, serviceId);
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
+
+        return factory;
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory(ConnectionFactory activeMQConnFactory) {
+        ConnectionFactory factory = new CachingConnectionFactory(activeMQConnFactory);
+
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter) {
+        JmsTemplate template = new JmsTemplate(connectionFactory);
+        template.setReceiveTimeout(receiveTimeout);
+        template.setMessageConverter(messageConverter);
+
+        return template;
+    }
+
+    @Bean
+    @Profile("jms-async")
+    public MessageListener messageListener() {
+        return serverComponent();
+    }
+
+    @Bean
+    @Profile("jms-async")
+    public SimpleMessageListenerContainer messageListenerContainer(
+            ActiveMQConnectionFactory activeMQConnectionFactory,
+            MessageListener messageListener) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(activeMQConnectionFactory);
+        container.setDestinationName(checkDestination);
+        container.setMessageListener(messageListener);
+
+        return container;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("typeId");
+
+        return messageConverter;
+    }
 }
--- server/src/main/java/dvoraka/avservice/server/configuration/JmsConfig.java
+++ server/src/main/java/dvoraka/avservice/server/configuration/JmsConfig.java
@@ -2,23 +2,12 @@ package dvoraka.avservice.server.configuration;
 
 import dvoraka.avservice.server.AvServer;
 import dvoraka.avservice.server.BasicAvServer;
-import dvoraka.avservice.server.ServerComponent;
 import dvoraka.avservice.server.jms.JmsClient;
-import dvoraka.avservice.server.jms.JmsComponent;
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jms.connection.CachingConnectionFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.listener.SimpleMessageListenerContainer;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-
-import javax.jms.ConnectionFactory;
-import javax.jms.MessageListener;
 
 /**
  * JMS configuration.
@@ -28,18 +17,6 @@ import javax.jms.MessageListener;
 @Profile("jms")
 public class JmsConfig {
 
-    @Value("${avservice.jms.brokerUrl}")
-    private String brokerUrl;
-
-    @Value("${avservice.jms.checkDestination:check}")
-    private String checkDestination;
-
-    @Value("${avservice.jms.resultDestination:result}")
-    private String resultDestination;
-
-    @Value("${avservice.jms.receiveTimeout:2000}")
-    private long receiveTimeout;
-
     @Value("${avservice.serviceId:default1")
     private String serviceId;
 
@@ -47,59 +24,6 @@ public class JmsConfig {
     @Bean
     public AvServer avServer() {
         return new BasicAvServer(serviceId);
-    }
-
-    @Bean
-    public ActiveMQConnectionFactory activeMQConnFactory() {
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
-
-        return factory;
-    }
-
-    @Bean
-    public ServerComponent serverComponent() {
-        return new JmsComponent(resultDestination, serviceId);
-    }
-
-    @Bean
-    @Profile("jms-async")
-    public SimpleMessageListenerContainer messageListenerContainer() {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(activeMQConnFactory());
-        container.setDestinationName(checkDestination);
-        container.setMessageListener(messageListener());
-
-        return container;
-    }
-
-    @Bean
-    public ConnectionFactory connectionFactory(ConnectionFactory activeMQConnFactory) {
-        ConnectionFactory factory = new CachingConnectionFactory(activeMQConnFactory);
-
-        return factory;
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
-        JmsTemplate template = new JmsTemplate(connectionFactory);
-        template.setReceiveTimeout(receiveTimeout);
-        template.setMessageConverter(messageConverter());
-
-        return template;
-    }
-
-    @Bean
-    public MessageConverter messageConverter() {
-        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
-        messageConverter.setTypeIdPropertyName("typeId");
-
-        return messageConverter;
-    }
-
-    @Bean
-    @Profile("jms-async")
-    public MessageListener messageListener() {
-        return serverComponent();
     }
 
     @Bean
