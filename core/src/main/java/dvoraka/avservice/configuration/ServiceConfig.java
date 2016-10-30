c8ec213815779f8c977f75acea59e84ef3998ba1
Alen Dvorak <alen.dvorak@gmail.com>
2016-10-30 18:05:14 +0100

    #102 Move MBean exporter configuration back

--- core/src/main/java/dvoraka/avservice/configuration/ServiceConfig.java
+++ core/src/main/java/dvoraka/avservice/configuration/ServiceConfig.java
@@ -6,20 +6,15 @@ import dvoraka.avservice.db.configuration.NoDatabaseConfig;
 import dvoraka.avservice.db.configuration.SolrConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.jmx.export.MBeanExporter;
-import org.springframework.jmx.export.annotation.AnnotationMBeanExporter;
-import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * AV service Spring configuration.
  */
 @Configuration
 @PropertySource("classpath:avservice.properties")
-@EnableMBeanExport
 @Import({
         ServiceCoreConfig.class,
         AvProgramConfig.class,
@@ -27,15 +22,8 @@ import org.springframework.jmx.support.RegistrationPolicy;
         NoDatabaseConfig.class,
         SolrConfig.class
 })
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class ServiceConfig {
-
-    @Bean
-    public MBeanExporter mbeanExporter() {
-        MBeanExporter exporter = new AnnotationMBeanExporter();
-        exporter.setRegistrationPolicy(RegistrationPolicy.REPLACE_EXISTING);
-
-        return exporter;
-    }
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
--- core/src/main/java/dvoraka/avservice/configuration/ServiceCoreConfig.java
+++ core/src/main/java/dvoraka/avservice/configuration/ServiceCoreConfig.java
@@ -9,12 +9,17 @@ import dvoraka.avservice.service.DefaultAvService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.jmx.export.annotation.AnnotationMBeanExporter;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * Core configuration
  */
 @Configuration
+@EnableMBeanExport
 @Profile("core")
 public class ServiceCoreConfig {
 
@@ -24,6 +29,14 @@ public class ServiceCoreConfig {
     @Value("${avservice.serviceId:default1}")
     private String serviceId;
 
+
+    @Bean
+    public MBeanExporter mbeanExporter() {
+        MBeanExporter exporter = new AnnotationMBeanExporter();
+        exporter.setRegistrationPolicy(RegistrationPolicy.REPLACE_EXISTING);
+
+        return exporter;
+    }
 
     @Bean
     public AvService avService(AvProgram avProgram) {
