faa32fee8a2ebc2b242b6684bd8ea938aed7e403
Alen Dvorak <alen.dvorak@gmail.com>
2016-11-18 21:40:00 +0100

    #109 Add interfaces

---
+++ common/src/main/java/dvoraka/avservice/common/PerformanceTest.java
@@ -0,0 +1,9 @@
+package dvoraka.avservice.common;
+
+/**
+ * Interface for performance tests.
+ */
+public interface PerformanceTest extends Test {
+
+    long getResult();
+}
---
+++ common/src/main/java/dvoraka/avservice/common/Test.java
@@ -0,0 +1,26 @@
+package dvoraka.avservice.common;
+
+/**
+ * Interface for tests.
+ */
+public interface Test {
+
+    /**
+     * Runs test.
+     */
+    void run();
+
+    /**
+     * Returns if the test is done.
+     *
+     * @return true if the test execution ended
+     */
+    boolean isDone();
+
+    /**
+     * Returns a test passed status.
+     *
+     * @return true if the test passed the test criteria
+     */
+    boolean passed();
+}
