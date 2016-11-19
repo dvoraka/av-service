96dc7cd26e05c98f5e0a5b455b42ae80fa8d764c
Alen Dvorak <alen.dvorak@gmail.com>
2016-11-19 13:34:24 +0100

    #111 Remove JMS client with its configuration

--- server/src/main/java/dvoraka/avservice/server/configuration/jms/JmsConfig.java
+++ server/src/main/java/dvoraka/avservice/server/configuration/jms/JmsConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Profile;
 @Import({
         JmsCommonConfig.class,
         JmsServerConfig.class,
-        JmsClientConfig.class,
         JmsCheckerConfig.class
 })
 @Profile("jms")
