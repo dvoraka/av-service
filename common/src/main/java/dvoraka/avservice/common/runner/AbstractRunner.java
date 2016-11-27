79b715c35c84e5fc0491fa7d95a1738e1c2a6112
Alen Dvorak <alen.dvorak@gmail.com>
2016-11-27 14:56:03 +0100

    #96 Add documentation for Runner

--- common/src/main/java/dvoraka/avservice/common/runner/AbstractRunner.java
+++ common/src/main/java/dvoraka/avservice/common/runner/AbstractRunner.java
@@ -19,6 +19,11 @@ public abstract class AbstractRunner implements Runner {
     protected Logger log = LogManager.getLogger(this.getClass().getName());
 
 
+    /**
+     * Returns a configured Spring application context for the runner.
+     *
+     * @return the application context
+     */
     protected AnnotationConfigApplicationContext applicationContext() {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext();
@@ -29,18 +34,36 @@ public abstract class AbstractRunner implements Runner {
         return context;
     }
 
+    /**
+     * Returns Spring profiles in an array.
+     *
+     * @return the profiles array
+     */
     protected String[] profiles() {
         return new String[]{"default"};
     }
 
+    /**
+     * Returns all configuration classes for the context in an array.
+     *
+     * @return the configuration classes array
+     */
     protected abstract Class<?>[] configClasses();
 
+    /**
+     * Returns a service running class.
+     *
+     * @return the run class
+     */
     protected abstract Class<? extends ServiceManagement> runClass();
 
     protected String stopMessage() {
         return "Press Enter to stop.";
     }
 
+    /**
+     * Runs the runner.
+     */
     @Override
     public void run() {
         AnnotationConfigApplicationContext context = applicationContext();
@@ -67,10 +90,21 @@ public abstract class AbstractRunner implements Runner {
         return running;
     }
 
+    /**
+     * Sets a running status.
+     *
+     * @param running the running flag
+     */
     protected void setRunning(boolean running) {
         this.running = running;
     }
 
+    /**
+     * Waiting for a key after runner is started. You can override it if you don't
+     * need any action with empty implementation.
+     *
+     * @throws IOException if reading from keyboard problem occurs
+     */
     protected void waitForKey() throws IOException {
         if (!testRun) {
             System.out.println(stopMessage());
@@ -78,10 +112,20 @@ public abstract class AbstractRunner implements Runner {
         }
     }
 
+    /**
+     * Special testing flag for not waiting on keyboard after start.
+     *
+     * @return if the run is only for testing
+     */
     public static boolean isTestRun() {
         return testRun;
     }
 
+    /**
+     * Setting a testing flag.
+     *
+     * @param testRun the testing flag
+     */
     public static void setTestRun(boolean testRun) {
         AbstractRunner.testRun = testRun;
     }
