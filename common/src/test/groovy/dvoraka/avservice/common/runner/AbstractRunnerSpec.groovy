b5bd64935963e4184e61dd991414a3a4ab5d27df
Alen Dvorak <alen.dvorak@gmail.com>
2016-12-03 21:06:25 +0100

    #68 Improve AbstractRunner test

--- common/src/test/groovy/dvoraka/avservice/common/runner/AbstractRunnerSpec.groovy
+++ common/src/test/groovy/dvoraka/avservice/common/runner/AbstractRunnerSpec.groovy
@@ -22,7 +22,7 @@ class AbstractRunnerSpec extends Specification {
             String[] profiles = ['test1', 'test2']
             runner.profiles() >> profiles
 
-            Class<?>[] classes = [AbstractRunnerSpec]
+            Class<?>[] classes = [AbstractRunnerSpec.class]
             runner.configClasses() >> classes
 
         when:
@@ -30,5 +30,52 @@ class AbstractRunnerSpec extends Specification {
 
         then:
             Arrays.equals(context.getEnvironment().getActiveProfiles(), profiles)
+    }
+
+    def "default profiles"() {
+        expect:
+            Arrays.equals(runner.profiles(), ['default'] as String[])
+
+    }
+
+    def "default stop message"() {
+        expect:
+            runner.stopMessage() == 'Press Enter to stop.'
+    }
+
+    def "runner status before start"() {
+        expect:
+            !runner.isRunning()
+            !runner.isStopped()
+    }
+
+    def "runner status after stop"() {
+        when:
+            runner.stop()
+
+        then:
+            runner.isStopped()
+    }
+
+    def "set running status"() {
+        when:
+            runner.setRunning(false)
+
+        then:
+            !runner.isRunning()
+
+        when:
+            runner.setRunning(true)
+
+        then:
+            runner.isRunning()
+    }
+
+    def "set test run"() {
+        when:
+            AbstractRunner.setTestRun(true)
+
+        then:
+            AbstractRunner.isTestRun()
     }
 }
