282412220a3f3f557165df1ef1492876c980dd16
Alen Dvorak <alen.dvorak@gmail.com>
2016-12-04 16:46:09 +0100

    #27 Prepare AMQP REST strategy

--- rest/src/main/java/dvoraka/avservice/rest/configuration/RestAmqpConfig.java
+++ rest/src/main/java/dvoraka/avservice/rest/configuration/RestAmqpConfig.java
@@ -2,6 +2,7 @@ package dvoraka.avservice.rest.configuration;
 
 import dvoraka.avservice.rest.AmqpRestStrategy;
 import dvoraka.avservice.rest.RestStrategy;
+import dvoraka.avservice.server.ServerComponent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Profile;
 public class RestAmqpConfig {
 
     @Bean
-    public RestStrategy restStrategy() {
-        return new AmqpRestStrategy(null);
+    public RestStrategy restStrategy(ServerComponent serverComponent) {
+        return new AmqpRestStrategy(serverComponent);
     }
 }
--- rest/src/main/java/dvoraka/avservice/rest/configuration/SpringWebConfig.java
+++ rest/src/main/java/dvoraka/avservice/rest/configuration/SpringWebConfig.java
@@ -4,6 +4,7 @@ import dvoraka.avservice.configuration.ServiceConfig;
 import dvoraka.avservice.rest.controller.AvRestController;
 import dvoraka.avservice.rest.service.DefaultRestService;
 import dvoraka.avservice.rest.service.RestService;
+import dvoraka.avservice.server.configuration.amqp.AmqpConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -21,7 +22,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
         RestLocalConfig.class,
         RestAmqpConfig.class,
         RestSecurityConfig.class,
-        ServiceConfig.class
+        ServiceConfig.class,
+        AmqpConfig.class
 })
 public class SpringWebConfig extends WebMvcConfigurerAdapter {
 
