2381deb2b6284d1745b5476bdefabab2643f6538
Alen Dvorak <alen.dvorak@gmail.com>
2016-12-29 15:02:00 +0100

    #63 Prepare statistics module

---
+++ stats/src/main/java/dvoraka/avservice/stats/Stats.java
@@ -0,0 +1,10 @@
+package dvoraka.avservice.stats;
+
+
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class Stats {
+
+    public static void main(String[] args) {
+        System.out.println("Stats module.");
+    }
+}
