04ce889a56dab55996f4495ece0ebb90cccef402
Alen Dvorak <alen.dvorak@gmail.com>
2017-01-01 20:53:40 +0100

    #135 REST app over Boot

---
+++ rest/src/main/java/dvoraka/avservice/rest/Application.java
@@ -0,0 +1,21 @@
+package dvoraka.avservice.rest;
+
+import dvoraka.avservice.rest.configuration.SpringWebConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Spring Boot application for REST.
+ */
+@EnableAutoConfiguration
+@Configuration
+@Import(SpringWebConfig.class)
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
