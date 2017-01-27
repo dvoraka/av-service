7f1a2a8c3af73fe7ce60ad74a14222ff2fac1bff
Alen Dvorak <alen.dvorak@gmail.com>
2017-01-27 12:50:30 +0100

    #164 Improve documentation

--- core/src/main/java/dvoraka/avservice/DefaultMessageProcessor.java
+++ core/src/main/java/dvoraka/avservice/DefaultMessageProcessor.java
@@ -28,7 +28,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * Main AV message processor.
+ * AV message processor. Checks messages for viruses.
  */
 @Service
 @ManagedResource
--- core/src/main/java/dvoraka/avservice/service/AvService.java
+++ core/src/main/java/dvoraka/avservice/service/AvService.java
@@ -9,20 +9,40 @@ import java.io.File;
  */
 public interface AvService {
 
+    /**
+     * Scans bytes and returns result as a boolean value.
+     *
+     * @param bytes the bytes
+     * @return the scan result
+     * @throws ScanErrorException if scan fails
+     */
     boolean scanBytes(byte[] bytes) throws ScanErrorException;
 
     /**
      * Scans bytes and returns result as a string.
+     * <p>
      * Value of {@link dvoraka.avservice.common.Utils#OK_VIRUS_INFO} is used
      * for the OK response string.
      *
      * @param bytes the bytes to scan
      * @return the result string
-     * @throws ScanErrorException if scan failed
+     * @throws ScanErrorException if scan fails
      */
     String scanBytesWithInfo(byte[] bytes) throws ScanErrorException;
 
+    /**
+     * Scans file and returns result as a boolean value.
+     *
+     * @param file the file for the scan
+     * @return the result of scan
+     * @throws ScanErrorException if scan fails
+     */
     boolean scanFile(File file) throws ScanErrorException;
 
+    /**
+     * Returns the maximum size of a file.
+     *
+     * @return the max size
+     */
     long getMaxFileSize();
 }
