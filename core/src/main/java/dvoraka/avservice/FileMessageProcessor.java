084078ecbb97f3f5558d0ff3d0eac13152ca4c09
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-02 15:26:42 +0100

    #155 Prepare file MP

--- core/src/main/java/dvoraka/avservice/FileMessageProcessor.java
+++ core/src/main/java/dvoraka/avservice/FileMessageProcessor.java
@@ -6,11 +6,16 @@ import dvoraka.avservice.common.data.FileMessage;
 import dvoraka.avservice.common.data.MessageStatus;
 import dvoraka.avservice.common.data.MessageType;
 import dvoraka.avservice.storage.service.FileService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
 
 /**
  * Message processor for processing files.
@@ -20,28 +25,41 @@ public class FileMessageProcessor implements MessageProcessor {
 
     private final FileService fileService;
 
+    private static final Logger log = LogManager.getLogger(FileMessageProcessor.class);
+
     private final List<AvMessageListener> listeners;
+    private final Map<MessageType, Consumer<AvMessage>> processMap;
 
 
     @Autowired
     public FileMessageProcessor(FileService fileService) {
         this.fileService = fileService;
         listeners = new CopyOnWriteArrayList<>();
+        processMap = getCallConfiguration();
+    }
+
+    private Map<MessageType, Consumer<AvMessage>> getCallConfiguration() {
+        Map<MessageType, Consumer<AvMessage>> configuration = new HashMap<>();
+        configuration.put(MessageType.FILE_SAVE, this::safe);
+        configuration.put(MessageType.FILE_LOAD, this::load);
+
+        return configuration;
     }
 
     @Override
     public void sendMessage(AvMessage message) {
+        processMap.get(message.getType())
+                .accept(message);
+    }
 
-        if (message.getType() == MessageType.FILE_SAVE) {
+    private void safe(AvMessage message) {
+        fileService.saveFile(message);
+        notifyListeners(listeners, message.createFileResponse(new byte[0]));
+    }
 
-            fileService.saveFile(message);
-            notifyListeners(listeners, message.createResponse("NNNNN"));
-
-        } else if (message.getType() == MessageType.FILE_LOAD) {
-
-            FileMessage fileMessage = fileService.loadFile(message);
-            notifyListeners(listeners, message.createFileResponse(fileMessage.getData()));
-        }
+    private void load(AvMessage message) {
+        FileMessage fileMessage = fileService.loadFile(message);
+        notifyListeners(listeners, message.createFileResponse(fileMessage.getData()));
     }
 
     @Override
