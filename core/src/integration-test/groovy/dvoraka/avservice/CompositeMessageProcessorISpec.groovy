a8583afd6cd8de7ba0145602d66269a9b4d04132
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-06 14:59:50 +0100

    #155 Improve test

--- core/src/integration-test/groovy/dvoraka/avservice/CompositeMessageProcessorISpec.groovy
+++ core/src/integration-test/groovy/dvoraka/avservice/CompositeMessageProcessorISpec.groovy
@@ -3,12 +3,16 @@ package dvoraka.avservice
 import dvoraka.avservice.common.AvMessageListener
 import dvoraka.avservice.common.Utils
 import dvoraka.avservice.common.data.AvMessage
+import dvoraka.avservice.common.data.MessageType
 import dvoraka.avservice.configuration.ServiceConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 import spock.util.concurrent.PollingConditions
+
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
 
 /**
  * CompositeMessageProcessor spec.
@@ -20,34 +24,38 @@ class CompositeMessageProcessorISpec extends Specification {
     @Autowired
     MessageProcessor checkAndFileProcessor
 
+    BlockingQueue<AvMessage> queue
+    AvMessageListener listener
+
     PollingConditions conditions
 
 
     def setup() {
-        conditions = new PollingConditions(timeout: 2)
+        queue = new ArrayBlockingQueue<>(10)
+        listener = new AvMessageListener() {
+            @Override
+            void onAvMessage(AvMessage message) {
+                queue.put(message)
+            }
+        }
 
+        checkAndFileProcessor.addProcessedAVMessageListener(listener)
+
+        conditions = new PollingConditions(timeout: 2)
     }
 
     def "test file checking"() {
         given:
             AvMessage message = Utils.genInfectedMessage()
 
-            AvMessage response = null
-            AvMessageListener messageListener = new AvMessageListener() {
-                @Override
-                void onAvMessage(AvMessage m) {
-                    response = m
-                }
-            }
-
-            checkAndFileProcessor.addProcessedAVMessageListener(messageListener)
-
         when:
             checkAndFileProcessor.sendMessage(message)
 
         then:
             conditions.eventually {
+                AvMessage response = queue.take()
                 response != null
+                response.getType() == MessageType.RESPONSE
                 response.getVirusInfo() != Utils.OK_VIRUS_INFO
                 response.getCorrelationId() == message.getId()
             }
