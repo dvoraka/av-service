f71ab1b75cee8aac11cfefaf44aec3b9938033d4
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-19 18:36:09 +0100

    #174 Disable load test ITs

--- server/src/integration-test/groovy/dvoraka/avservice/server/runner/amqp/AmqpLoadTestRunnerISpec.groovy
+++ server/src/integration-test/groovy/dvoraka/avservice/server/runner/amqp/AmqpLoadTestRunnerISpec.groovy
@@ -1,10 +1,12 @@
 package dvoraka.avservice.server.runner.amqp
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * Test for load test running.
  */
+@Ignore
 class AmqpLoadTestRunnerISpec extends Specification {
 
     def "Run AMQP load test runner"() {
--- server/src/integration-test/groovy/dvoraka/avservice/server/runner/jms/JmsLoadTestRunnerISpec.groovy
+++ server/src/integration-test/groovy/dvoraka/avservice/server/runner/jms/JmsLoadTestRunnerISpec.groovy
@@ -1,10 +1,12 @@
 package dvoraka.avservice.server.runner.jms
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * Test for load test running.
  */
+@Ignore
 class JmsLoadTestRunnerISpec extends Specification {
 
     def "Run JMS load test runner"() {
