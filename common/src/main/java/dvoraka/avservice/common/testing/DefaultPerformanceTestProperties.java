fe0303a1bf229c07cb3fc42f02637a44b6171274
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-19 23:37:22 +0100

    #172 Remove unused code

--- common/src/main/java/dvoraka/avservice/common/testing/DefaultPerformanceTestProperties.java
+++ common/src/main/java/dvoraka/avservice/common/testing/DefaultPerformanceTestProperties.java
@@ -1,19 +1,12 @@
 package dvoraka.avservice.common.testing;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.Map;
-
 /**
  * Default performance test properties implementation.
  */
 public final class DefaultPerformanceTestProperties implements PerformanceTestProperties {
 
-    private static final Logger log = LogManager.getLogger(DefaultPerformanceTestProperties.class);
-
-    private long msgCount;
-    private boolean sendOnly;
+    private final long msgCount;
+    private final boolean sendOnly;
 
 
     private DefaultPerformanceTestProperties(Builder builder) {
@@ -26,36 +19,14 @@ public final class DefaultPerformanceTestProperties implements PerformanceTestPr
         return msgCount;
     }
 
-    private void setMsgCount(long count) {
-        msgCount = count;
-    }
-
     @Override
     public boolean isSendOnly() {
         return sendOnly;
     }
 
-    private void setSendOnly(boolean sendOnly) {
-        this.sendOnly = sendOnly;
-    }
-
-    /**
-     * Loads properties from the map.
-     *
-     * @param props the properties
-     */
-    private void loadProperties(Map<String, String> props) {
-        if (props.containsKey("messageCount")) {
-            setMsgCount(Integer.parseInt(props.get("messageCount")));
-        }
-        if (props.containsKey("sendOnly")) {
-            setSendOnly(Boolean.parseBoolean(props.get("sendOnly")));
-        }
-    }
-
     @Override
     public String toString() {
-        return "BasicLoadTestProperties{"
+        return "DefaultPerformanceTestProperties{"
                 + ", msgCount=" + msgCount
                 + ", sendOnly=" + sendOnly
                 + '}';
@@ -68,6 +39,7 @@ public final class DefaultPerformanceTestProperties implements PerformanceTestPr
 
         private long msgCount;
         private boolean sendOnly;
+
 
         public Builder() {
             this.msgCount = 1;
