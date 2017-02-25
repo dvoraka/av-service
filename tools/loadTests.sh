61aa5c9aed1fda2df5f8b123b713f2fbacde3816
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-25 19:45:10 +0100

    #182 Improve load test script

--- tools/loadTests.sh
+++ tools/loadTests.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
+#
+# Runs AMQP and JMS load tests. The default is 3 loops and you can give another count
+# as a first program's argument.
+#
 
 SRC_ROOT=".."
 
+loops=3
+
 
 cd ${SRC_ROOT}
-
-loops=3
 
 function runTask {
     for loop in `seq $1`
     do
         ./gradlew -q $2 2> /dev/null
-        if [ $? != 0 ]
+        if [ $? -ne 0 ]
         then
+            echo "Test failed for: "$2
             exit 1
         fi
         echo "--------"
     done
 }
+
+if [ $# -ne 1 ]
+then
+    loops=3
+else
+    loops=$1
+fi
 
 echo "Run AMQP load test with ${loops} loops..."
 runTask ${loops} runAmqpLoadTest
