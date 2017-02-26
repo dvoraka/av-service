c65df26a76566ea4777758447e8a765367864698
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-26 14:30:00 +0100

    #177 Improve tests

---
+++ database/src/integration-test/groovy/dvoraka/avservice/db/configuration/DatabaseConfigISpec.groovy
@@ -0,0 +1,24 @@
+package dvoraka.avservice.db.configuration
+
+import dvoraka.avservice.db.service.MessageInfoService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * Configuration test.
+ */
+@ContextConfiguration(classes = [DatabaseConfig.class])
+@ActiveProfiles(['db'])
+class DatabaseConfigISpec extends Specification {
+
+    @Autowired
+    MessageInfoService service
+
+
+    def "test"() {
+        expect:
+            true
+    }
+}
---
+++ database/src/test/groovy/dvoraka/avservice/db/model/FileSpec.groovy
@@ -0,0 +1,68 @@
+package dvoraka.avservice.db.model
+
+import dvoraka.avservice.common.data.FileMessage
+import spock.lang.Specification
+import spock.lang.Subject
+
+/**
+ * File spec.
+ */
+class FileSpec extends Specification {
+
+    @Subject
+    File file
+
+
+    def setup() {
+        file = new File()
+    }
+
+    def "set and get"() {
+        given:
+            Long dbId = 9
+            byte[] data = new byte[10]
+            String filename = 'testFilename'
+            String owner = 'testOwner'
+
+        when:
+            file.with {
+                setId(dbId)
+                setData(data)
+                setFilename(filename)
+            }
+            file.setOwner(owner)
+
+        then:
+            with(file) {
+                getId() == dbId
+                Arrays.equals(getData(), data)
+                getFilename() == filename
+            }
+            file.getOwner() == owner
+    }
+
+    def "create file message"() {
+        given:
+            Long dbId = 9
+            byte[] data = new byte[10]
+            String filename = 'testFilename'
+            String owner = 'testOwner'
+            String testCorrId = 'testId'
+
+        when:
+            file.with {
+                setId(dbId)
+                setData(data)
+                setFilename(filename)
+            }
+            file.setOwner(owner)
+
+            FileMessage fileMessage = file.fileMessage(testCorrId)
+
+        then:
+            fileMessage.getCorrelationId() == testCorrId
+            Arrays.equals(fileMessage.getData(), data)
+            fileMessage.getFilename() == filename
+            fileMessage.getOwner() == owner
+    }
+}
