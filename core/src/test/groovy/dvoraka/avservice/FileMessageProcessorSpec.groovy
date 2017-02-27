f84a3b4aabea7a99346fa9feb4bbcbfd71421f9a
Alen Dvorak <alen.dvorak@gmail.com>
2017-02-27 13:59:43 +0100

    #177 Improve tests

--- core/src/test/groovy/dvoraka/avservice/FileMessageProcessorSpec.groovy
+++ core/src/test/groovy/dvoraka/avservice/FileMessageProcessorSpec.groovy
@@ -1,5 +1,6 @@
 package dvoraka.avservice
 
+import dvoraka.avservice.common.AvMessageListener
 import dvoraka.avservice.common.Utils
 import dvoraka.avservice.common.data.AvMessage
 import dvoraka.avservice.storage.service.FileService
@@ -15,21 +16,37 @@ class FileMessageProcessorSpec extends Specification {
     FileMessageProcessor processor
 
     FileService service
+    AvMessageListener listener
 
 
     def setup() {
         service = Mock()
+        listener = Mock()
         processor = new FileMessageProcessor(service)
+        processor.addProcessedAVMessageListener(listener)
     }
 
     def "save message"() {
         given:
-            AvMessage message = Utils.genFileMessage()
+            AvMessage message = Utils.genSaveMessage()
 
         when:
             processor.sendMessage(message)
 
         then:
             1 * service.saveFile(message)
+            1 * listener.onAvMessage(_)
+    }
+
+    def "load message"() {
+        given:
+            AvMessage message = Utils.genLoadMessage()
+
+        when:
+            processor.sendMessage(message)
+
+        then:
+            1 * service.loadFile(_) >> message
+            1 * listener.onAvMessage(_)
     }
 }
