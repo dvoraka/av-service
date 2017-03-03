8d5a55d63375f058d7073853290021af1bce871e
Alen Dvorak <alen.dvorak@gmail.com>
2017-03-03 15:23:19 +0100

    #189 Add dummy user service

---
+++ user/src/main/java/dvoraka/avservice/user/service/DummyUserService.java
@@ -0,0 +1,15 @@
+package dvoraka.avservice.user.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Dummy user service.
+ */
+public class DummyUserService implements UserService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        throw new UsernameNotFoundException("User not found: " + username);
+    }
+}
