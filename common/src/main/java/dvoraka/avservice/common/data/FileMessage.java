64caf53c803d86afd5f52f8726eeb7577086811a
Alen Dvorak <alen.dvorak@gmail.com>
2017-03-04 15:57:40 +0100

    #183 Update AV message doc

--- common/src/main/java/dvoraka/avservice/common/data/FileMessage.java
+++ common/src/main/java/dvoraka/avservice/common/data/FileMessage.java
@@ -5,9 +5,24 @@ package dvoraka.avservice.common.data;
  */
 public interface FileMessage extends Message {
 
+    /**
+     * Returns a message data.
+     *
+     * @return the data
+     */
     byte[] getData();
 
+    /**
+     * Returns a filename for a data in a message.
+     *
+     * @return the filename
+     */
     String getFilename();
 
+    /**
+     * Returns an owner of a data in a message.
+     *
+     * @return the data owner
+     */
     String getOwner();
 }
--- common/src/main/java/dvoraka/avservice/common/data/Message.java
+++ common/src/main/java/dvoraka/avservice/common/data/Message.java
@@ -5,9 +5,24 @@ package dvoraka.avservice.common.data;
  */
 public interface Message {
 
+    /**
+     * Returns ID of a message.
+     *
+     * @return the message ID
+     */
     String getId();
 
+    /**
+     * Returns a correlation ID of a message.
+     *
+     * @return the message correlation ID
+     */
     String getCorrelationId();
 
+    /**
+     * Returns a message type.
+     *
+     * @return the message type
+     */
     MessageType getType();
 }
