14db6d295dd080531bacfe7cc85f89a7cf6d04f1
Alen Dvorak <alen.dvorak@gmail.com>
2017-03-05 15:15:14 +0100

    #190 Prepare infrastructure

---
+++ common/src/main/java/dvoraka/avservice/common/service/MessageStatusStorage.java
@@ -0,0 +1,15 @@
+package dvoraka.avservice.common.service;
+
+import dvoraka.avservice.common.data.MessageStatus;
+
+/**
+ * Interface for message status storage.
+ */
+public interface MessageStatusStorage {
+
+    void started(String id);
+
+    void processed(String id);
+
+    MessageStatus getStatus(String id);
+}
