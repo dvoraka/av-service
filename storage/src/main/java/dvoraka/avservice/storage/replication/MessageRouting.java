5d827c98c0eb04b945a794c925739adea6f3bace
Alen Dvorak <alen.dvorak@gmail.com>
2017-03-12 15:03:54 +0100

    #193 Prepare message

---
+++ storage/src/main/java/dvoraka/avservice/storage/replication/MessageRouting.java
@@ -0,0 +1,9 @@
+package dvoraka.avservice.storage.replication;
+
+/**
+ * Message routing type.
+ */
+public enum MessageRouting {
+    BROADCAST,
+    UNICAST
+}
--- storage/src/main/java/dvoraka/avservice/storage/replication/ReplicationMessage.java
+++ storage/src/main/java/dvoraka/avservice/storage/replication/ReplicationMessage.java
@@ -6,4 +6,10 @@ import dvoraka.avservice.common.data.FileMessage;
  * Replication message.
  */
 public interface ReplicationMessage extends FileMessage {
+
+    String getFromId();
+
+    String getToId();
+
+    MessageRouting getRouting();
 }
