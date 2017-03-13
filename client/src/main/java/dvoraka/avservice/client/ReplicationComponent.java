09598c8335dd4114759fea5ccf90ad501966e2a3
Alen Dvorak <alen.dvorak@gmail.com>
2017-03-13 19:10:05 +0100

    #192 Prepare infrastructure

--- client/src/main/java/dvoraka/avservice/client/ReplicationComponent.java
+++ client/src/main/java/dvoraka/avservice/client/ReplicationComponent.java
@@ -3,14 +3,29 @@ package dvoraka.avservice.client;
 import dvoraka.avservice.common.ReplicationMessageListener;
 import dvoraka.avservice.common.data.ReplicationMessage;
 
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
 /**
  * Component for sending a receiving replication messages.
  */
-public interface ReplicationComponent {
+public interface ReplicationComponent extends
+        MessageListener,
+        org.springframework.amqp.core.MessageListener {
 
     void sendMessage(ReplicationMessage message);
 
     void addReplicationMessageListener(ReplicationMessageListener listener);
 
     void removeReplicationMessageListener(ReplicationMessageListener listener);
+
+    @Override
+    default void onMessage(Message message) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default void onMessage(org.springframework.amqp.core.Message message) {
+        throw new UnsupportedOperationException();
+    }
 }
