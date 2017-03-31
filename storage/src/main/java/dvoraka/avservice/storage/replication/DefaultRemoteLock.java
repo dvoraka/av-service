a7bf5ab099e40964837503fe089ea8727dd28e5c
Alen Dvorak <alen.dvorak@gmail.com>
2017-03-31 18:03:54 +0200

    #216 Prepare lock implementation

--- storage/src/main/java/dvoraka/avservice/storage/replication/DefaultRemoteLock.java
+++ storage/src/main/java/dvoraka/avservice/storage/replication/DefaultRemoteLock.java
@@ -2,19 +2,24 @@ package dvoraka.avservice.storage.replication;
 
 import dvoraka.avservice.client.service.ReplicationServiceClient;
 import dvoraka.avservice.client.service.response.ReplicationResponseClient;
+import dvoraka.avservice.common.ReplicationMessageListener;
+import dvoraka.avservice.common.data.ReplicationMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Default remote lock implementation.
  */
 @Component
-public class DefaultRemoteLock implements RemoteLock {
+public class DefaultRemoteLock implements RemoteLock, ReplicationMessageListener {
 
     private final ReplicationServiceClient serviceClient;
     private final ReplicationResponseClient responseClient;
 
-//    private long sequence;
+    private AtomicLong sequence;
 
 
     @Autowired
@@ -24,6 +29,14 @@ public class DefaultRemoteLock implements RemoteLock {
     ) {
         this.serviceClient = serviceClient;
         this.responseClient = responseClient;
+
+        sequence = new AtomicLong();
+    }
+
+    @PostConstruct
+    public void start() {
+        responseClient.addNoResponseMessageListener(this);
+        initializeSequence();
     }
 
     @Override
@@ -32,6 +45,8 @@ public class DefaultRemoteLock implements RemoteLock {
         // send lock sequence query
         //
         // get actual lock sequence or create new one
+        initializeSequence();
+
         //
         // send lock query
         //
@@ -51,5 +66,30 @@ public class DefaultRemoteLock implements RemoteLock {
         // get unlock query responses
 
         return true;
+    }
+
+    private void initializeSequence() {
+        serviceClient.sendMessage(null); // sequence request
+
+        // read response
+
+        // set sequence
+    }
+
+    public long getSequence() {
+        return sequence.get();
+    }
+
+    private void setSequence(long sequence) {
+        this.sequence.set(sequence);
+    }
+
+    private void incSequence() {
+        sequence.getAndIncrement();
+    }
+
+    @Override
+    public void onMessage(ReplicationMessage message) {
+
     }
 }
