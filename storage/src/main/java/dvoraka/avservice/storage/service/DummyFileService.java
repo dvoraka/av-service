616529b7fa404746fc3ca23f86419ad1347e9ddc
Alen Dvorak <alen.dvorak@gmail.com>
2017-04-11 15:04:46 +0200

    #192 Add log output to dummy file service

--- storage/src/main/java/dvoraka/avservice/storage/service/DummyFileService.java
+++ storage/src/main/java/dvoraka/avservice/storage/service/DummyFileService.java
@@ -1,34 +1,41 @@
 package dvoraka.avservice.storage.service;
 
 import dvoraka.avservice.common.data.FileMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Dummy file service implementation.
  */
 public class DummyFileService implements FileService {
 
+    private static final Logger log = LogManager.getLogger(DummyFileService.class);
+
+
     @Override
     public void saveFile(FileMessage message) {
-        // do nothing
+        log.info("Save: {}", message);
     }
 
     @Override
     public FileMessage loadFile(FileMessage message) {
+        log.info("Load: {}", message);
         return null;
     }
 
     @Override
     public void updateFile(FileMessage message) {
-        // do nothing
+        log.info("Update: {}", message);
     }
 
     @Override
     public void deleteFile(FileMessage message) {
-        // do nothing
+        log.info("Delete: {}", message);
     }
 
     @Override
     public boolean exists(String filename, String owner) {
+        log.info("Exists: {}, {}", filename, owner);
         return false;
     }
 }
