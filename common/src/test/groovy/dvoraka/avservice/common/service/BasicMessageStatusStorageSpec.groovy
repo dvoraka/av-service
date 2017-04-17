87f7efffb833d38250faa750fe23dc424e3c8d74
Alen Dvorak <alen.dvorak@gmail.com>
2017-04-17 21:46:31 +0200

    #225 Improve tests

--- common/src/test/groovy/dvoraka/avservice/common/service/BasicMessageStatusStorageSpec.groovy
+++ common/src/test/groovy/dvoraka/avservice/common/service/BasicMessageStatusStorageSpec.groovy
@@ -1,9 +1,51 @@
 package dvoraka.avservice.common.service
 
+import dvoraka.avservice.common.data.MessageStatus
 import spock.lang.Specification
+import spock.lang.Subject
 
 /**
  * BasicMessageStatusStorage spec.
  */
 class BasicMessageStatusStorageSpec extends Specification {
+
+    @Subject
+    BasicMessageStatusStorage storage
+
+
+    def setup() {
+        storage = new BasicMessageStatusStorage(2_000L)
+    }
+
+    def cleanup() {
+        storage.stop()
+    }
+
+    def "test storage"() {
+        given:
+            String testId = 'test'
+
+        when:
+            storage.started(testId)
+
+        then:
+            storage.getStatus(testId) == MessageStatus.PROCESSING
+
+        when:
+            storage.processed(testId)
+
+        then:
+            storage.getStatus(testId) == MessageStatus.PROCESSED
+
+        when:
+            storage.stop()
+
+        then:
+            notThrown(Exception)
+    }
+
+    def "get status for unknown ID"() {
+        expect:
+            storage.getStatus('unknown') == MessageStatus.UNKNOWN
+    }
 }
