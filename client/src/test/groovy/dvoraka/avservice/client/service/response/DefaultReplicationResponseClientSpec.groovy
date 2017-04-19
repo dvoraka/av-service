b1e63550631b9c6655721e3b5c11fa05ca862d46
Alen Dvorak <alen.dvorak@gmail.com>
2017-04-19 18:05:09 +0200

    #225 Improve tests

--- client/src/test/groovy/dvoraka/avservice/client/service/response/DefaultReplicationResponseClientSpec.groovy
+++ client/src/test/groovy/dvoraka/avservice/client/service/response/DefaultReplicationResponseClientSpec.groovy
@@ -1,13 +1,15 @@
 package dvoraka.avservice.client.service.response
 
 import dvoraka.avservice.client.ReplicationComponent
+import dvoraka.avservice.common.data.ReplicationMessage
+import dvoraka.avservice.common.replication.ReplicationHelper
 import spock.lang.Specification
 import spock.lang.Subject
 
 /**
  * DefaultReplicationResponseClient spec.
  */
-class DefaultReplicationResponseClientSpec extends Specification {
+class DefaultReplicationResponseClientSpec extends Specification implements ReplicationHelper {
 
     @Subject
     DefaultReplicationResponseClient client
@@ -20,9 +22,17 @@ class DefaultReplicationResponseClientSpec extends Specification {
         replicationComponent = Mock()
 
         client = new DefaultReplicationResponseClient(replicationComponent, nodeId)
+        client.start()
+    }
+
+    def cleanup() {
+        client.stop()
     }
 
     def "start and stop"() {
+        given:
+            client = new DefaultReplicationResponseClient(replicationComponent, nodeId)
+
         when:
             client.start()
 
@@ -46,5 +56,17 @@ class DefaultReplicationResponseClientSpec extends Specification {
 
         then:
             !client.isStarted()
+    }
+
+    def "processing message - response"() {
+        given:
+            ReplicationMessage request = createDiscoverRequest(nodeId)
+            ReplicationMessage reply = createDiscoverReply(request, 'otherId')
+
+        when:
+            client.onMessage(reply)
+
+        then:
+            client.getResponse(request.getId())
     }
 }
