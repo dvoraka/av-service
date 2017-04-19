60ebe09347c7983172aa6ed472014ebf58c996d6
Alen Dvorak <alen.dvorak@gmail.com>
2017-04-19 16:41:09 +0200

    #225 Improve tests

---
+++ client/src/integration-test/groovy/dvoraka/avservice/client/configuration/TestAmqpReplicationClientConfigISpec.groovy
@@ -0,0 +1,26 @@
+package dvoraka.avservice.client.configuration
+
+import dvoraka.avservice.client.service.ReplicationServiceClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * Configuration test.
+ */
+@ContextConfiguration(classes = [ClientConfig.class])
+@ActiveProfiles(['client', 'replication-test', 'amqp', 'amqp-client', 'no-db'])
+@DirtiesContext
+class TestAmqpReplicationClientConfigISpec extends Specification {
+
+    @Autowired
+    ReplicationServiceClient client
+
+
+    def "test"() {
+        expect:
+            true
+    }
+}
