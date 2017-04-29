4b08a6f96a18d1043721f499d82fd2b4fd27656c
Alen Dvorak <alen.dvorak@gmail.com>
2017-04-29 14:52:46 +0200

    #225 Enable reconfigured tests

--- server/src/integration-test/groovy/dvoraka/avservice/server/runner/amqp/AmqpLoadTestRunnerISpec.groovy
+++ server/src/integration-test/groovy/dvoraka/avservice/server/runner/amqp/AmqpLoadTestRunnerISpec.groovy
@@ -1,13 +1,15 @@
 package dvoraka.avservice.server.runner.amqp
 
-import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * Test for load test running.
  */
-@Ignore
 class AmqpLoadTestRunnerISpec extends Specification {
+
+    def setupSpec() {
+        System.setProperty('avservice.perf.msgCount', '2')
+    }
 
     def "Run AMQP load test runner"() {
         when:
--- server/src/integration-test/groovy/dvoraka/avservice/server/runner/jms/JmsLoadTestRunnerISpec.groovy
+++ server/src/integration-test/groovy/dvoraka/avservice/server/runner/jms/JmsLoadTestRunnerISpec.groovy
@@ -1,13 +1,15 @@
 package dvoraka.avservice.server.runner.jms
 
-import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * Test for load test running.
  */
-@Ignore
 class JmsLoadTestRunnerISpec extends Specification {
+
+    def setupSpec() {
+        System.setProperty('avservice.perf.msgCount', '2')
+    }
 
     def "Run JMS load test runner"() {
         when:
