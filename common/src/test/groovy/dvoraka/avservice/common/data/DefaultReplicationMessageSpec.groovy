7da9d58632a5adb8ed14f324f968fa9c77b80cbb
Alen Dvorak <alen.dvorak@gmail.com>
2017-05-04 22:05:57 +0200

    #225 Improve test

--- common/src/test/groovy/dvoraka/avservice/common/data/DefaultReplicationMessageSpec.groovy
+++ common/src/test/groovy/dvoraka/avservice/common/data/DefaultReplicationMessageSpec.groovy
@@ -11,19 +11,40 @@ class DefaultReplicationMessageSpec extends Specification {
         given:
             String testCorrId = 'corrId'
             MessageType testType = MessageType.REPLICATION_SERVICE
+
             byte[] testData = new byte[10]
+            String testFilename = 'testFilename'
+            String testOwner = 'testOwner'
+
+            String testFromId = 'testFromId'
+            String testToId = 'testToId'
+            long testSequence = 999L
 
         when:
             ReplicationMessage message = new DefaultReplicationMessage.Builder(null)
                     .correlationId(testCorrId)
                     .type(testType)
+
                     .data(testData)
+                    .filename(testFilename)
+                    .owner(testOwner)
+
+                    .fromId(testFromId)
+                    .toId(testToId)
+                    .sequence(testSequence)
                     .build()
 
         then:
             message.getId()
             message.getCorrelationId() == testCorrId
             message.getType() == testType
+
             Arrays.equals(message.getData(), new byte[10])
+            message.getFilename() == testFilename
+            message.getOwner() == testOwner
+
+            message.getFromId() == testFromId
+            message.getToId() == testToId
+            message.getSequence() == testSequence
     }
 }
