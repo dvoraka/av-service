a3b1f9eba857f610ec426f18ce596ce9a0a85762
Alen Dvorak <alen.dvorak@gmail.com>
2017-05-24 16:59:33 +0200

    #245 Update replication tests

---
+++ storage/src/integration-test/groovy/dvoraka/avservice/storage/configuration/AmqpReplicationServiceConfigISpec.groovy
@@ -0,0 +1,24 @@
+package dvoraka.avservice.storage.configuration
+
+import dvoraka.avservice.storage.replication.ReplicationService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * AMQP replication service configuration spec.
+ */
+@ContextConfiguration(classes = [StorageConfig.class])
+@ActiveProfiles(['storage', 'replication', 'client', 'amqp', 'no-db'])
+class AmqpReplicationServiceConfigISpec extends Specification {
+
+    @Autowired
+    ReplicationService service
+
+
+    def "test"() {
+        expect:
+            true
+    }
+}
--- storage/src/integration-test/groovy/dvoraka/avservice/storage/replication/ReplicationServiceISpec.groovy
+++ storage/src/integration-test/groovy/dvoraka/avservice/storage/replication/ReplicationServiceISpec.groovy
@@ -1,18 +1,19 @@
 package dvoraka.avservice.storage.replication
 
 import dvoraka.avservice.client.QueueCleaner
-import dvoraka.avservice.common.Utils
 import dvoraka.avservice.storage.configuration.StorageConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.PropertySource
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
-import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * AMQP replication service spec.
+ *
+ * Complete test is in the server module. This could be for some testing without a server
+ * infrastructure.
  */
 @ContextConfiguration(classes = [StorageConfig.class])
 @PropertySource("classpath:avservice.properties")
@@ -30,17 +31,12 @@ class ReplicationServiceISpec extends Specification {
 
 
     def cleanup() {
+        // clean the replication queue
         queueCleaner.clean(queueName)
     }
 
     def "test"() {
         expect:
             true
-    }
-
-    @Ignore('WIP')
-    def "save file"() {
-        expect:
-            service.saveFile(Utils.genSaveMessage())
     }
 }
