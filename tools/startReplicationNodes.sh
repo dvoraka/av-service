8b9ed126ff60b71efa4c80e92b29a339643227b7
Alen Dvorak <alen.dvorak@gmail.com>
2017-05-27 00:23:48 +0200

    #248 Improve script

--- tools/startReplicationNodes.sh
+++ tools/startReplicationNodes.sh
@@ -2,6 +2,10 @@
 #
 # Runs replication nodes.
 #
+# First argument is number of containers (default 2).
+#
+# IMAGE is Docker image name.
+#
 
 IMAGE='replication_node'
 
@@ -13,17 +17,25 @@ else
 fi
 
 start=500
-end=$((500 + count - 1))
+end=$(($start + count - 1))
 
 
+echo 'Starting nodes:'
 for i in $(seq ${start} ${end})
 do
-    echo -n "Starting node${i}... "
+    echo -n " * node${i}... "
     docker run --network=host -d -e "AVSERVICE_STORAGE_REPLICATION_NODEID=node${i}" \
         ${IMAGE} 2>&1 > /dev/null
     echo 'OK'
+
+    sleep 3
 done
 
-read -p 'Press enter to stop nodes' variable
+echo
 
+read -p 'Press enter to stop the nodes' _
+echo
+
+echo -n 'Stopping nodes... '
 docker kill $(docker ps -f ancestor=${IMAGE} -q) 2>&1 > /dev/null
+echo 'OK'
