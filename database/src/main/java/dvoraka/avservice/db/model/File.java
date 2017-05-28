2c0e3e2a18e6ba4194e10cc0cd92ae2cf9c33c53
Alen Dvorak <alen.dvorak@gmail.com>
2017-05-28 14:53:23 +0200

    #245 Add unique constraint to File entity

--- database/src/main/java/dvoraka/avservice/db/model/File.java
+++ database/src/main/java/dvoraka/avservice/db/model/File.java
@@ -8,11 +8,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.UUID;
 
 /**
  * File entity.
  */
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"filename", "owner"})
+        }
+)
 @Entity
 public class File {
 
