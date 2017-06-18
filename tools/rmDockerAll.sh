f6e1c5c6788fbe829579ed54efef1f65ce3b187f
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-18 16:30:56 +0200

    #96 Update script

--- tools/rmDockerAll.sh
+++ tools/rmDockerAll.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 #
-# Docker commands for cleaning space on a disk
+# Tool for removing all Docker containers and images.
 #
 
 # Delete all containers
+echo "Removing all containers..."
 docker rm $(docker ps -a -q)
+echo "Done."
 
 # Delete all images
+echo "Removing all images..."
 docker rmi $(docker images -q)
+echo "Done."
