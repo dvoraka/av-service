2a45685a60e2e86204a964167043b28b43430e8d
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-22 15:24:30 +0200

    #264 Add new script for rebuilding dev images

---
+++ tools/rebuildDevImages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Rebuilds development Docker images.
+#
+
+IMAGES="replication-node"
+DOCKER_DIR=~/projects/av-service/docker
+
+
+echo "Removing images..."
+for IMAGE in ${IMAGES}
+do
+    docker rmi ${IMAGE}
+    sleep 4
+done
+echo "Done."
+echo
+
+echo "Building replication node..."
+cd ${DOCKER_DIR}/replicationnodesnap
+docker build -t replication-node .
+echo "Done."
