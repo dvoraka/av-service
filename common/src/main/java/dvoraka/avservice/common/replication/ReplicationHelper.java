b08e9765e9200d1400b74559f0cddf4693a115dd
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-24 15:45:09 +0200

    #96 Prepare refactoring

--- common/src/main/java/dvoraka/avservice/common/replication/ReplicationHelper.java
+++ common/src/main/java/dvoraka/avservice/common/replication/ReplicationHelper.java
@@ -14,13 +14,27 @@ import dvoraka.avservice.common.data.ReplicationStatus;
 //TODO: refactoring
 public interface ReplicationHelper extends ReplicationServiceHelper {
 
-    default ReplicationMessage createSaveMessage(
-            FileMessage message, String fromNode, String toNode
+    default ReplicationMessage createNoDataMessage(
+            FileMessage message, Command command, String fromNode, String toNode
     ) {
         return new DefaultReplicationMessage.Builder(message.getId())
                 .type(MessageType.REPLICATION_COMMAND)
                 .routing(MessageRouting.UNICAST)
-                .command(Command.SAVE)
+                .command(command)
+                .toId(toNode)
+                .fromId(fromNode)
+                .filename(message.getFilename())
+                .owner(message.getOwner())
+                .build();
+    }
+
+    default ReplicationMessage createDataMessage(
+            FileMessage message, Command command, String fromNode, String toNode
+    ) {
+        return new DefaultReplicationMessage.Builder(message.getId())
+                .type(MessageType.REPLICATION_COMMAND)
+                .routing(MessageRouting.UNICAST)
+                .command(command)
                 .toId(toNode)
                 .fromId(fromNode)
                 .data(message.getData())
@@ -29,18 +43,28 @@ public interface ReplicationHelper extends ReplicationServiceHelper {
                 .build();
     }
 
-    default ReplicationMessage createSaveSuccess(ReplicationMessage message, String fromNode) {
+    default ReplicationMessage createSaveMessage(
+            FileMessage message, String fromNode, String toNode
+    ) {
+        return createDataMessage(message, Command.SAVE, fromNode, toNode);
+    }
+
+    default ReplicationMessage createSuccessResponse(ReplicationMessage message, String fromNode) {
         return new DefaultReplicationMessage.Builder(null)
                 .correlationId(message.getId())
                 .type(MessageType.REPLICATION_COMMAND)
                 .routing(MessageRouting.UNICAST)
-                .command(Command.SAVE)
+                .command(message.getCommand())
                 .replicationStatus(ReplicationStatus.OK)
                 .toId(message.getFromId())
                 .fromId(fromNode)
                 .filename(message.getFilename())
                 .owner(message.getOwner())
                 .build();
+    }
+
+    default ReplicationMessage createSaveSuccess(ReplicationMessage message, String fromNode) {
+        return createSuccessResponse(message, fromNode);
     }
 
     default ReplicationMessage createSaveFailed(ReplicationMessage message, String fromNode) {
@@ -60,15 +84,7 @@ public interface ReplicationHelper extends ReplicationServiceHelper {
     default ReplicationMessage createLoadMessage(
             FileMessage message, String fromNode, String toNode
     ) {
-        return new DefaultReplicationMessage.Builder(message.getId())
-                .type(MessageType.REPLICATION_COMMAND)
-                .routing(MessageRouting.UNICAST)
-                .command(Command.LOAD)
-                .toId(toNode)
-                .fromId(fromNode)
-                .filename(message.getFilename())
-                .owner(message.getOwner())
-                .build();
+        return createNoDataMessage(message, Command.LOAD, fromNode, toNode);
     }
 
     default ReplicationMessage createLoadSuccess(
@@ -107,30 +123,13 @@ public interface ReplicationHelper extends ReplicationServiceHelper {
     default ReplicationMessage createUpdateMessage(
             FileMessage message, String fromNode, String toNode
     ) {
-        return new DefaultReplicationMessage.Builder(null)
-                .type(MessageType.REPLICATION_COMMAND)
-                .routing(MessageRouting.UNICAST)
-                .command(Command.UPDATE)
-                .toId(toNode)
-                .fromId(fromNode)
-                .data(message.getData())
-                .filename(message.getFilename())
-                .owner(message.getOwner())
-                .build();
+        return createDataMessage(message, Command.UPDATE, fromNode, toNode);
     }
 
     default ReplicationMessage createDeleteMessage(
             FileMessage message, String fromNode, String toNode
     ) {
-        return new DefaultReplicationMessage.Builder(message.getId())
-                .type(MessageType.REPLICATION_COMMAND)
-                .routing(MessageRouting.UNICAST)
-                .command(Command.DELETE)
-                .toId(toNode)
-                .fromId(fromNode)
-                .filename(message.getFilename())
-                .owner(message.getOwner())
-                .build();
+        return createNoDataMessage(message, Command.DELETE, fromNode, toNode);
     }
 
     default ReplicationMessage createDeleteSuccess(
