f0e78d014f79f63f89b95e43299a2b7a076bd4fc
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-24 22:10:41 +0200

    #96 Improve tests

--- core/src/test/groovy/dvoraka/avservice/core/FileMessageProcessorSpec.groovy
+++ core/src/test/groovy/dvoraka/avservice/core/FileMessageProcessorSpec.groovy
@@ -4,6 +4,8 @@ import dvoraka.avservice.common.AvMessageListener
 import dvoraka.avservice.common.Utils
 import dvoraka.avservice.common.data.AvMessage
 import dvoraka.avservice.common.data.MessageStatus
+import dvoraka.avservice.common.data.MessageType
+import dvoraka.avservice.storage.exception.FileServiceException
 import dvoraka.avservice.storage.service.FileService
 import spock.lang.Specification
 import spock.lang.Subject
@@ -43,6 +45,20 @@ class FileMessageProcessorSpec extends Specification {
             1 * listener.onAvMessage(_)
     }
 
+    def "save message failed"() {
+        given:
+            AvMessage message = Utils.genSaveMessage()
+
+        when:
+            processor.sendMessage(message)
+
+        then:
+            1 * service.saveFile(_) >> {
+                throw new FileServiceException()
+            }
+            1 * listener.onAvMessage(_)
+    }
+
     def "load message"() {
         given:
             AvMessage message = Utils.genLoadMessage()
@@ -52,6 +68,20 @@ class FileMessageProcessorSpec extends Specification {
 
         then:
             1 * service.loadFile(_) >> message
+            1 * listener.onAvMessage(_)
+    }
+
+    def "load message failed"() {
+        given:
+            AvMessage message = Utils.genLoadMessage()
+
+        when:
+            processor.sendMessage(message)
+
+        then:
+            1 * service.loadFile(_) >> {
+                throw new FileServiceException()
+            }
             1 * listener.onAvMessage(_)
     }
 
@@ -67,6 +97,20 @@ class FileMessageProcessorSpec extends Specification {
             1 * listener.onAvMessage(_)
     }
 
+    def "update message failed"() {
+        given:
+            AvMessage message = Utils.genUpdateMessage()
+
+        when:
+            processor.sendMessage(message)
+
+        then:
+            1 * service.updateFile(_) >> {
+                throw new FileServiceException()
+            }
+            1 * listener.onAvMessage(_)
+    }
+
     def "delete message"() {
         given:
             AvMessage message = Utils.genDeleteMessage()
@@ -76,6 +120,20 @@ class FileMessageProcessorSpec extends Specification {
 
         then:
             1 * service.deleteFile(_)
+            1 * listener.onAvMessage(_)
+    }
+
+    def "delete message failed"() {
+        given:
+            AvMessage message = Utils.genDeleteMessage()
+
+        when:
+            processor.sendMessage(message)
+
+        then:
+            1 * service.deleteFile(_) >> {
+                throw new FileServiceException()
+            }
             1 * listener.onAvMessage(_)
     }
 
@@ -115,5 +173,23 @@ class FileMessageProcessorSpec extends Specification {
         then:
             notThrown(Exception)
             0 * _
+    }
+
+    def "set input filter"() {
+        given:
+            AvMessage saveMessage = Utils.genSaveMessage()
+
+        when:
+            processor.sendMessage(saveMessage)
+
+        then:
+            1 * service._
+
+        when:
+            processor.setInputFilter({ msg -> msg.getType() == MessageType.FILE_LOAD })
+            processor.sendMessage(saveMessage)
+
+        then:
+            0 * service._
     }
 }
