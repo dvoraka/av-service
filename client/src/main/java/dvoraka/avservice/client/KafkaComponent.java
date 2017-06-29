34069be1cf9b468c65733d619dc0af45924a2324
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-30 01:03:04 +0200

    #104 Prepare infrastructure

--- client/src/main/java/dvoraka/avservice/client/KafkaComponent.java
+++ client/src/main/java/dvoraka/avservice/client/KafkaComponent.java
@@ -2,13 +2,16 @@ package dvoraka.avservice.client;
 
 import dvoraka.avservice.common.AvMessageListener;
 import dvoraka.avservice.common.data.AvMessage;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * Kafka component.
  */
+@Component
 public class KafkaComponent implements ServerComponent {
 
     private final KafkaTemplate<String, AvMessage> kafkaTemplate;
@@ -36,5 +39,10 @@ public class KafkaComponent implements ServerComponent {
     @Override
     public void removeAvMessageListener(AvMessageListener listener) {
 
+    }
+
+    @KafkaListener(topics = "avcheck.t")
+    public void onMessage(AvMessage message) {
+        System.out.println(message);
     }
 }
--- client/src/main/java/dvoraka/avservice/client/configuration/KafkaClientConfig.java
+++ client/src/main/java/dvoraka/avservice/client/configuration/KafkaClientConfig.java
@@ -3,14 +3,22 @@ package dvoraka.avservice.client.configuration;
 import dvoraka.avservice.client.KafkaComponent;
 import dvoraka.avservice.client.ServerComponent;
 import dvoraka.avservice.common.data.AvMessage;
+import dvoraka.avservice.common.data.DefaultAvMessage;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -20,6 +28,7 @@ import java.util.Map;
  * Kafka configuration.
  */
 @Configuration
+@EnableKafka
 @Profile("kafka")
 public class KafkaClientConfig {
 
@@ -32,7 +41,6 @@ public class KafkaClientConfig {
 
     @Bean
     public ProducerFactory<String, AvMessage> producerFactory() {
-
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9099");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -45,4 +53,39 @@ public class KafkaClientConfig {
     public KafkaTemplate<String, AvMessage> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9099");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "json");
+
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, DefaultAvMessage> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                consumerConfigs(),
+                new StringDeserializer(),
+                new JsonDeserializer<>(DefaultAvMessage.class)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, DefaultAvMessage>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, DefaultAvMessage> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
+
+//    @Bean
+//    public MessageListener<String, AvMessage> messageListener(ServerComponent serverComponent) {
+//        return serverComponent;
+//    }
 }
