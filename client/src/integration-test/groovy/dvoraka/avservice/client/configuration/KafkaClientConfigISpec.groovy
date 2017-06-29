2cf9b59cff2973c7eb70c324a69207472c09a2d1
Alen Dvorak <alen.dvorak@gmail.com>
2017-06-29 17:04:47 +0200

    #104 Prepare infrastructure

--- client/src/integration-test/groovy/dvoraka/avservice/client/configuration/KafkaClientConfigISpec.groovy
+++ client/src/integration-test/groovy/dvoraka/avservice/client/configuration/KafkaClientConfigISpec.groovy
@@ -1,5 +1,7 @@
 package dvoraka.avservice.client.configuration
 
+import dvoraka.avservice.common.Utils
+import dvoraka.avservice.common.data.AvMessage
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.annotation.DirtiesContext
@@ -19,7 +21,7 @@ class KafkaClientConfigISpec extends Specification {
 //    ServerComponent serverComponent
 
     @Autowired
-    KafkaTemplate<String, String> kafkaTemplate
+    KafkaTemplate<String, AvMessage> kafkaTemplate
 
 
     def "test"() {
@@ -29,6 +31,6 @@ class KafkaClientConfigISpec extends Specification {
 
     def "send message"() {
         expect:
-            kafkaTemplate.send('test.t', 'Hello')
+            kafkaTemplate.send('test.t', Utils.genMessage())
     }
 }
--- client/src/main/java/dvoraka/avservice/client/configuration/KafkaClientConfig.java
+++ client/src/main/java/dvoraka/avservice/client/configuration/KafkaClientConfig.java
@@ -1,5 +1,6 @@
 package dvoraka.avservice.client.configuration;
 
+import dvoraka.avservice.common.data.AvMessage;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,24 +22,18 @@ import java.util.Map;
 public class KafkaClientConfig {
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, AvMessage> producerFactory() {
 
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9099");
-        configProps.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        configProps.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9099");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, AvMessage> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
