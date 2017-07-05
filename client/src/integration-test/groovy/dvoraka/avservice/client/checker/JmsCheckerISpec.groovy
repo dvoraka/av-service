0954bf78a6e8b12d4d2567101ab9cd245527f31a
Alen Dvorak <alen.dvorak@gmail.com>
2017-07-05 20:29:51 +0200

    #270 Tuning

--- client/src/integration-test/groovy/dvoraka/avservice/client/checker/JmsCheckerISpec.groovy
+++ client/src/integration-test/groovy/dvoraka/avservice/client/checker/JmsCheckerISpec.groovy
@@ -18,6 +18,6 @@ class JmsCheckerISpec extends CheckerISpec {
         JmsCheckServerRunner.setTestRun(false)
         runner = new JmsCheckServerRunner()
         runner.runAsync()
-        sleep(3_000)
+        sleep(5_000)
     }
 }
