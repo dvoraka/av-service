0d8ee3f573c6a7bc9ee7ad28a0035647e8cf198b
Alen Dvorak <alen.dvorak@gmail.com>
2017-07-18 22:23:21 +0200

    #271 Prepare runner configuration

--- runner/src/main/java/dvoraka/avservice/runner/RunnerConfiguration.java
+++ runner/src/main/java/dvoraka/avservice/runner/RunnerConfiguration.java
@@ -7,7 +7,24 @@ import dvoraka.avservice.common.runner.ServiceRunner;
  */
 public interface RunnerConfiguration {
 
+    /**
+     * Returns a configuration ID.
+     *
+     * @return the configuration ID
+     */
     String getId();
 
+    /**
+     * Returns a service runner.
+     *
+     * @return the service runner
+     */
     ServiceRunner getServiceRunner();
+
+    /**
+     * Check service running status. The real service check.
+     *
+     * @return the running status
+     */
+    boolean running();
 }
