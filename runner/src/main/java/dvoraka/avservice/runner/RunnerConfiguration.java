caceeacff3d10d8fc88b64ac13ee8bef8d5e0ae1
Alen Dvorak <alen.dvorak@gmail.com>
2017-07-18 22:11:23 +0200

    #271 Prepare runner configuration

--- runner/src/main/java/dvoraka/avservice/runner/RunnerConfiguration.java
+++ runner/src/main/java/dvoraka/avservice/runner/RunnerConfiguration.java
@@ -1,7 +1,13 @@
 package dvoraka.avservice.runner;
 
+import dvoraka.avservice.common.runner.ServiceRunner;
+
 /**
  * Runner configuration.
  */
 public interface RunnerConfiguration {
+
+    String getId();
+
+    ServiceRunner getServiceRunner();
 }
