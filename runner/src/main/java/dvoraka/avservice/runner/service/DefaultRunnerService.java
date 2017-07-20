2dbd206c9ccf43e23b06ca0332fa1039d979ccd2
Alen Dvorak <alen.dvorak@gmail.com>
2017-07-20 22:34:00 +0200

    #271 Prepare implementation

--- runner/src/main/java/dvoraka/avservice/runner/service/DefaultRunnerService.java
+++ runner/src/main/java/dvoraka/avservice/runner/service/DefaultRunnerService.java
@@ -3,14 +3,31 @@ package dvoraka.avservice.runner.service;
 import dvoraka.avservice.runner.RunnerAlreadyExists;
 import dvoraka.avservice.runner.RunnerConfiguration;
 import dvoraka.avservice.runner.RunningState;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Default runner service implementation.
  */
+@Service
 public class DefaultRunnerService implements RunnerService {
+
+    private final ConcurrentMap<String, RunnerConfiguration> configurations;
+
+
+    public DefaultRunnerService() {
+        configurations = new ConcurrentHashMap<>();
+    }
 
     @Override
     public void create(RunnerConfiguration configuration) throws RunnerAlreadyExists {
+        if (configurations.containsKey(configuration.getId())) {
+            throw new RunnerAlreadyExists();
+        }
+
+        configurations.put(configuration.getId(), configuration);
     }
 
     @Override
