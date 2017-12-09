391053ece72221488976864e779773b15aab3f47
dvoraka <alen.dvorak@gmail.com>
2017-12-10 00:28:59 +0100

    #96 Prepare new abstraction for network components

---
+++ client/src/main/java/dvoraka/avservice/client/AbstractNetworkComponent.java
@@ -0,0 +1,38 @@
+package dvoraka.avservice.client;
+
+import dvoraka.avservice.common.AvMessageListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Abstract base class for network components.
+ */
+public abstract class AbstractNetworkComponent implements NetworkComponent {
+
+    private final List<AvMessageListener> listeners;
+
+    protected final Logger log = LogManager.getLogger(this.getClass());
+
+
+    protected AbstractNetworkComponent() {
+        listeners = new CopyOnWriteArrayList<>();
+    }
+
+    @Override
+    public void addAvMessageListener(AvMessageListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void removeAvMessageListener(AvMessageListener listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public int getListenerCount() {
+        return listeners.size();
+    }
+}
