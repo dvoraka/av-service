987ed06d1b02b025add445a8bcbfcee198edd403
dvoraka <alen.dvorak@gmail.com>
2017-12-10 16:35:20 +0100

    #96 Migrate JMS adapter to new design

--- client/src/main/java/dvoraka/avservice/client/jms/JmsAdapter.java
+++ client/src/main/java/dvoraka/avservice/client/jms/JmsAdapter.java
@@ -1,13 +1,10 @@
 package dvoraka.avservice.client.jms;
 
-import dvoraka.avservice.client.NetworkComponent;
-import dvoraka.avservice.common.AvMessageListener;
+import dvoraka.avservice.client.AbstractNetworkComponent;
 import dvoraka.avservice.common.data.AvMessage;
 import dvoraka.avservice.common.data.InfoSource;
 import dvoraka.avservice.common.helper.AvMessageHelper;
 import dvoraka.avservice.db.service.MessageInfoService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.JmsException;
 import org.springframework.jms.core.JmsTemplate;
@@ -17,8 +14,6 @@ import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import static java.util.Objects.requireNonNull;
 
@@ -26,16 +21,13 @@ import static java.util.Objects.requireNonNull;
  * JMS network component adapter.
  */
 @Component
-public class JmsAdapter implements NetworkComponent, AvMessageHelper {
+public class JmsAdapter extends AbstractNetworkComponent implements AvMessageHelper {
 
     private final JmsTemplate jmsTemplate;
     private final MessageInfoService messageInfoService;
 
-    private static final Logger log = LogManager.getLogger(JmsAdapter.class);
-
     private final String destination;
     private final String serviceId;
-    private final List<AvMessageListener> listeners;
     private final MessageConverter messageConverter;
 
 
@@ -51,8 +43,6 @@ public class JmsAdapter implements NetworkComponent, AvMessageHelper {
         this.jmsTemplate = requireNonNull(jmsTemplate);
         this.messageInfoService = requireNonNull(messageInfoService);
         messageConverter = requireNonNull(jmsTemplate.getMessageConverter());
-
-        listeners = new CopyOnWriteArrayList<>();
     }
 
     @Override
@@ -70,7 +60,7 @@ public class JmsAdapter implements NetworkComponent, AvMessageHelper {
             return;
         }
 
-        notifyListeners(listeners, avMessage);
+        notifyListeners(getListeners(), avMessage);
     }
 
     @Override
@@ -90,21 +80,6 @@ public class JmsAdapter implements NetworkComponent, AvMessageHelper {
         } catch (JmsException e) {
             log.warn("Message send problem!", e);
         }
-    }
-
-    @Override
-    public void addAvMessageListener(AvMessageListener listener) {
-        listeners.add(listener);
-    }
-
-    @Override
-    public void removeAvMessageListener(AvMessageListener listener) {
-        listeners.remove(listener);
-    }
-
-    @Override
-    public int getListenerCount() {
-        return listeners.size();
     }
 
     @Override
