f0cd0c87c27cc417da15b313ecd2e7bd8ba738cd
dvoraka <alen.dvorak@gmail.com>
2019-07-07 15:46:14 +0200

    #326 Prepare new service

---
+++ storage/src/main/java/dvoraka/avservice/storage/replication/NewReplicationService.java
@@ -0,0 +1,67 @@
+package dvoraka.avservice.storage.replication;
+
+import dvoraka.avservice.common.data.FileMessage;
+import dvoraka.avservice.common.data.replication.ReplicationMessage;
+import dvoraka.avservice.common.data.replication.ReplicationStatus;
+import dvoraka.avservice.storage.exception.FileServiceException;
+
+/**
+ * New replication service.
+ */
+public class NewReplicationService implements ReplicationService {
+
+    @Override
+    public ReplicationStatus getStatus(FileMessage message) {
+        return null;
+    }
+
+    @Override
+    public void setReplicationCount(int count) {
+
+    }
+
+    @Override
+    public int getMaxResponseTime() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxResponseTime(int maxTime) {
+
+    }
+
+    @Override
+    public void onMessage(ReplicationMessage message) {
+
+    }
+
+    @Override
+    public boolean isRunning() {
+        return false;
+    }
+
+    @Override
+    public void saveFile(FileMessage message) throws FileServiceException {
+
+    }
+
+    @Override
+    public FileMessage loadFile(FileMessage message) throws FileServiceException {
+        return null;
+    }
+
+    @Override
+    public void updateFile(FileMessage message) throws FileServiceException {
+
+    }
+
+    @Override
+    public void deleteFile(FileMessage message) throws FileServiceException {
+
+    }
+
+    @Override
+    public boolean exists(String filename, String owner) {
+        return false;
+    }
+}
