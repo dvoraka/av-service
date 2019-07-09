1896525430d3beee49178a2878d75c49edb23fb6
dvoraka <alen.dvorak@gmail.com>
2019-07-09 19:07:17 +0200

    #328 Prepare broker

---
+++ client/src/main/java/dvoraka/avservice/client/transport/test/DefaultSimpleBroker.java
@@ -0,0 +1,42 @@
+package dvoraka.avservice.client.transport.test;
+
+import dvoraka.avservice.common.data.replication.ReplicationMessage;
+import dvoraka.avservice.common.listener.MessageListener;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+
+public class DefaultSimpleBroker implements SimpleBroker<ReplicationMessage> {
+
+    private final Map<String, Set<MessageListener<ReplicationMessage>>> queueMap;
+
+
+    public DefaultSimpleBroker() {
+        queueMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public void send(String queueName, ReplicationMessage message) {
+
+        if (queueMap.get(queueName) != null) {
+            Set<MessageListener<ReplicationMessage>> listeners = queueMap.get(queueName);
+            listeners.forEach(listener -> listener.onMessage(message));
+        }
+    }
+
+    @Override
+    public void addMessageListener(MessageListener<ReplicationMessage> listener, String queueName) {
+
+        if (queueMap.get(queueName) == null) {
+            Set<MessageListener<ReplicationMessage>> listeners = new CopyOnWriteArraySet<>();
+            listeners.add(listener);
+            queueMap.put(queueName, listeners);
+        } else {
+            Set<MessageListener<ReplicationMessage>> listeners = queueMap.get(queueName);
+            listeners.add(listener);
+        }
+    }
+}
