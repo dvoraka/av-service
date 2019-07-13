4b349f52535521b60f190bdbfefe62d7be67f022
Alen Dvorak <alen.dvorak@gmail.com>
2019-07-13 14:52:21 +0200

    #326 Prepare response processing

---
+++ storage/src/main/java/dvoraka/avservice/storage/replication/ResponseLatch.java
@@ -0,0 +1,33 @@
+package dvoraka.avservice.storage.replication;
+
+import dvoraka.avservice.common.data.Message;
+
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class ResponseLatch<M extends Message> {
+
+    private final CountDownLatch latch;
+    private final String correlationID;
+
+    private Set<M> responses;
+
+
+    public ResponseLatch(String correlationID) {
+        this.correlationID = correlationID;
+        latch = new CountDownLatch(1);
+    }
+
+    public void await(long timeout) {
+        try {
+            latch.await(timeout, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCorrelationID() {
+        return correlationID;
+    }
+}
