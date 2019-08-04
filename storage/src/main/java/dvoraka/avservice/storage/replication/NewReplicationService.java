a4257a5372002f581c1717a6eea3c0abf5aa6797
dvoraka <alen.dvorak@gmail.com>
2019-08-05 00:55:31 +0200

    #326 Prepare dynamic node discovery

--- storage/src/main/java/dvoraka/avservice/storage/replication/NewReplicationService.java
+++ storage/src/main/java/dvoraka/avservice/storage/replication/NewReplicationService.java
@@ -4,6 +4,7 @@ import dvoraka.avservice.client.transport.ReplicationComponent;
 import dvoraka.avservice.common.data.FileMessage;
 import dvoraka.avservice.common.data.replication.ReplicationMessage;
 import dvoraka.avservice.common.data.replication.ReplicationStatus;
+import dvoraka.avservice.common.helper.replication.ReplicationHelper;
 import dvoraka.avservice.common.listener.ReplicationMessageListener;
 import dvoraka.avservice.common.service.TimedStorage;
 import dvoraka.avservice.storage.exception.FileAlreadyExistsException;
@@ -15,7 +16,11 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
@@ -24,12 +29,17 @@ import static java.util.Objects.requireNonNull;
  * New replication service.
  */
 @Slf4j
-public class NewReplicationService implements ReplicationService {
+public class NewReplicationService implements ReplicationService, ReplicationHelper {
 
     private final FileService fileService;
     private final ReplicationComponent component;
 
+    private static final int DISCOVERY_DELAY = 20_000;
+
     private final String nodeId;
+    private final Set<String> neighbours;
+    private final ScheduledExecutorService executorService;
+    private final String idString;
 
     private final TimedStorage<String> openFiles;
 
@@ -43,7 +53,11 @@ public class NewReplicationService implements ReplicationService {
 
         openFiles = new TimedStorage<>();
         messageListener = this;
+
         nodeId = this.component.getServiceId();
+        neighbours = new CopyOnWriteArraySet<>();
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        idString = "(" + nodeId + ")";
     }
 
     @PostConstruct
@@ -51,6 +65,8 @@ public class NewReplicationService implements ReplicationService {
     public void start() {
         component.addMessageListener(messageListener);
         running = true;
+
+        scheduleDiscovery();
     }
 
     @PreDestroy
@@ -58,6 +74,57 @@ public class NewReplicationService implements ReplicationService {
     public void stop() {
         component.removeMessageListener(messageListener);
         running = false;
+    }
+
+    private void scheduleDiscovery() {
+        executorService.scheduleWithFixedDelay(
+                this::discoverNeighboursSafe, 0, DISCOVERY_DELAY, TimeUnit.MILLISECONDS);
+    }
+
+    private void discoverNeighboursSafe() {
+        try {
+            discoverNeighbours();
+        } catch (Exception e) {
+            log.warn("Discovery failed!", e);
+        }
+    }
+
+    private void discoverNeighbours() {
+        log.debug("Discovering neighbours {}...", idString);
+
+        // old code
+//        ReplicationMessage message = createDiscoverRequest(nodeId);
+//        serviceClient.sendMessage(message);
+//
+//        Set<String> newNeighbours = responseClient
+//                .getResponseWait(message.getId(), maxResponseTime, maxResponseTime)
+//                .orElseGet(ReplicationMessageList::new)
+//                .stream()
+//                .filter(msg -> msg.getReplicationStatus() == ReplicationStatus.READY)
+//                .map(ReplicationMessage::getFromId)
+//                .collect(Collectors.toSet());
+//
+//        if (newNeighbours.isEmpty()) {
+//            log.debug("Neighbours discovered {}: none", idString);
+//        } else {
+//
+//            if (!neighbours.equals(newNeighbours)) {
+//                log.info("Neighbours changed {}: {} to {}",
+//                        idString, neighbours.size(), newNeighbours.size());
+//
+//                remoteLock.networkChanged();
+//            }
+//
+//            // TODO: improve synchronization
+//            neighbours.clear();
+//            neighbours.addAll(newNeighbours);
+//            log.debug("Neighbours discovered {}: {}", idString, neighbourCount());
+//        }
+//
+//        //TODO: if it's stopped it sets running flag again
+//        if (!isRunning()) {
+//            setRunning(true);
+//        }
     }
 
     @Override
